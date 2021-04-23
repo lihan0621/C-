ec5fe82c28855ef9ad9f1c3c4d7313d2df1bd953
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-23 13:33:47 +0800

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+char * reverse_string(char *str){
+	char *start = str;
+	char *end = str + strlen(str) - 1;
+	for (; start < end; start++, end--){
+		char ch = *start;
+		*start = *end;
+		*end = ch;
+	}
+	return str;
+}
+int main(){
+	char tmp[] = "hello bit!";
+	printf("%s\n", reverse_string(tmp));
+	system("pause");
+	return 0;
+}
