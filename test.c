2fe34770ae31763a59647ea871282d59858e9ad0
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-24 16:10:36 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,63 +1,63 @@
-#define  _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int Findkey(int arr[], int n, int key)
+int Add(int a, int b)
 {
-	for (int i = 0; i < n; i++) {
-		if (arr[i] == key) {
-			return i;
-		}
-	}
-	return -1;
+	return a + b;
 }
-int FindMax(int arr[], int n) {
-	//int max = 0;int arr[] = { -1, -2, -3, -4 };
-	int max = arr[0];
-	for (int i = 1; i < n; i++) {
-		if (arr[i] > max) {
-			max = arr[i];
-		}
-	}
-	return max;
-}
-int FindSecond(int arr[], int n)
+int Sub(int a, int b)
 {
-	int first;
-	int second;
-	if (arr[0] > arr[1]) {
-		first = arr[0];
-		second = arr[1];
-	}
-	if (arr[0] < arr[1]) {
-		first = arr[1];
-		second = arr[0];
-	}
-	for (int i = 2; i < n; i++) {
-		if (arr[i] > first) {
-			second = first;
-			first = arr[i];
-		}
-		else if (arr[i] > second) {
-			second = arr[i];
-		}
-	}
-	return second;
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	return a * b;
+}
+int Div(int a, int b)
+{
+	return a / b;
+}
+int Mod(int a, int b)
+{
+	return a % b;
 }
 int main()
 {
-	int arr[] = { 1,84,5,43,27,74,7,8,73,12,4,76 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	//int key = 0;
-	//scanf("%d", &key);
-	//int index = Findkey(arr, n, key);
-	//if (index == -1) {
-	//	printf("没找到\n");
-	//}
-	//else {
-	//	printf("%d\n", index);
-	//}
-	int max = FindMax(arr, n);
-	printf("%d\n", max);
-	int second = FindSecond(arr, n);
-	printf("%d\n", second);
+	int select = 1;
+	int op1, op2, result;
+	while (select) {
+		printf("***************************\n");
+		printf("*[1]Add       [2]Sub      *\n");
+		printf("*[3]Mul       [4]Div      *\n");
+		printf("*[5]Mod       [0]Quit     *\n");
+		printf("***************************\n");
+		printf("请选择:>");
+		scanf("%d", &select);
+		if (select == 0) 
+			break;
+		printf("请选择op1,op2:>");
+		scanf("%d,%d", &op1, &op2);
+		switch (select) {
+			case 1:
+				result = Add(op1, op2);
+				break;
+			case 2:
+				result = Sub(op1, op2);
+				break;
+			case 3:
+				result = Mul(op1, op2);
+				break;
+			case 4:
+				result = Div(op1, op2);
+				break;
+			case 5:
+				result = Mod(op1, op2);
+				break;
+			default:
+				printf("输入的cmd有误，请重新输入......\n");
+				break;
+		}
+		printf("result = %d\n", result);
+	}
+	printf("系统退出\n");
 	return 0;
 }
