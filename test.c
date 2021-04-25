43a8554d12ad072a8ad7d7ceddfdd792944e691d
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-25 20:26:56 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,32 +1,58 @@
 ﻿//#include<stdio.h>
 //#include<stdlib.h>
-//void Print(int arr[6]){
+//void Print(int arr)
+//{
+//	int i = 0;
 //	int* p = arr;
-//	for (int i = 0; i < 6; i++){
-//	printf("%d",*(p+i));
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
 //	}
+//	printf("\n");
 //}
-//int main(){
-//	int arr[6] = { 0, 1, 2, 3, 4, 5 };
-//	Print( arr[6]);
+//
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4,5 };
+//	Print(arr);
 //	system("pause");
 //	return 0;
 //}
-#include<stdio.h>
-﻿int PrintArr(int arr[])
-{
-	int i = 0;
-	int* p = arr;
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", *(p + i));
-	}
-}
-
-int main()
-{
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-	PrintArr(arr);
-
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a = 0;
+//	int num = 0;
+//	int sum = 0;
+//	for (int i = 0; i < 5; i++){
+//		a = a * 10 + 2;
+//		sum = sum + a;
+//	}
+//	printf("%d\n",sum);
+//	system("pause");
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<stdlib.h>
+//char * reverse_string(char *str){
+//	char *start = str;
+//	char *end = str + strlen(str) - 1;
+//	for (; start < end; start++, end--){
+//	char ch = *start;
+//	*start = *end;
+//	*end = ch;
+//	}
+//	return str;
+//}
+//int main(){
+//	char tmp[] = "hello bit!";
+//	reverse_string(tmp);
+//	printf("%s\n", tmp);
+	/*int a = 0x11223344;
+	char *pc = (char*)&a;
+	*pc = 0;
+	printf("%x\n", a);
+	system("pause");
 	return 0;
-}
+}*/
