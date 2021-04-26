672bcc7172b915e77d2d492ac8e5e94dc5cd05ae
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-26 18:29:38 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,39 +1,37 @@
-//#include<iostream>
-//#include<stdio.h>
-//using namespace std;
-//int main()
-//{
-//	int m = 0123;
-//	int n = 123;
-//	printf("%o %o\n", m, n);
-//	return 0;
-//}
-//#include<iostream>
-//using namespace std;
-//char fun(char x, char y)
-//{
-//	if (x < y)
-//		return x;
-//	return y;
-//}
-//int main()
-//{
-//	int a = '1', b = '1', c = '2';
-//	cout << fun(fun(a, b), fun(b, c));
-//	return 0;
-//}
 #include<iostream>
 using namespace std;
-int f(int n)
+class A
 {
-	if (n == 1)
-		return 1;
-	else
-		return (f(n - 1) + n * n * n);
+public:
+	A()
+	{
+		cout << "A::A()" << endl;
+	}
+	~A()
+	{
+		cout << "A::~A()" << endl;
+	}
+	int a = 0;
+};
+class B :public A
+{
+public:
+	B()
+	{
+		cout << "B::B()" << endl;
+	}
+	~B()
+	{
+		cout << "B::~B()" << endl;
+	}
+	int b = 0;
+};
+void f()
+{
+	B b;
 }
 int main()
 {
-	int s = f(3);
-	cout << s << endl;
+	f();
 	return 0;
 }
