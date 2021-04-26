88f4b3a23158412da1beb050688895bc064cd2c9
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-26 10:51:42 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,58 +1,33 @@
-﻿//#include<stdio.h>
-//#include<stdlib.h>
-//void Print(int arr)
+//#include<stdio.h>
+//int main()
 //{
-//	int i = 0;
-//	int* p = arr;
-//	for (i = 0; i < 10; i++)
-//	{
-//		printf("%d ", *(p + i));
-//	}
-//	printf("\n");
+//	int a = 1, b = 0, c = -1, d = 0;
+//	d = ++a || ++b && ++c;
+//	printf("%d\n", d);
+//	return 0;
 //}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0, a = 0, b = 2, c = 3, d = 4;
+//	//i = a++ && ++b && d++;
+//	i = a++||++b||d++;
+//	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a = 1, b = 2, c = 3, d = 0;
+//	if (a == 1 && b++ == 2)
+//		if (b != 2 || c-- != 3)
+//			printf("%d,%d,%d\n", a, b, c);
+//		else
+//			printf("%d,%d,%d\n", a, b, c);
+//	else
+//		printf("%d,%d,%d\n", a, b, c);
 //
-//int main()
-//{
-//	int arr[] = { 1, 2, 3, 4,5 };
-//	Print(arr);
-//	system("pause");
+//
 //	return 0;
 //}
-//#include<stdio.h>
-//#include<stdlib.h>
-//int main()
-//{
-//	int a = 0;
-//	int num = 0;
-//	int sum = 0;
-//	for (int i = 0; i < 5; i++){
-//		a = a * 10 + 2;
-//		sum = sum + a;
-//	}
-//	printf("%d\n",sum);
-//	system("pause");
-//	return 0;
-//}
-//#include<stdio.h>
-//#include<stdlib.h>
-//char * reverse_string(char *str){
-//	char *start = str;
-//	char *end = str + strlen(str) - 1;
-//	for (; start < end; start++, end--){
-//	char ch = *start;
-//	*start = *end;
-//	*end = ch;
-//	}
-//	return str;
-//}
-//int main(){
-//	char tmp[] = "hello bit!";
-//	reverse_string(tmp);
-//	printf("%s\n", tmp);
-	/*int a = 0x11223344;
-	char *pc = (char*)&a;
-	*pc = 0;
-	printf("%x\n", a);
-	system("pause");
-	return 0;
-}*/
