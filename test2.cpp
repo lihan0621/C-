a058a1f59b7342841ed7de194f0d2a451a87ed18
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-27 13:27:17 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,26 +1,44 @@
 #include<iostream>
-#include<string>
+#include<vector>
 using namespace std;
 int main()
 {
-	string str1, str2;
-	//cin >> str1;
-	//cin >> str2;  这里不能用cin，cin遇到空格就退出了
-	getline(cin, str1);
-	getline(cin, str2);
-	int hash[256] = { 0 };
-	string ret = "";
-	for (int i = 0; i < str2.size(); i++)
+	int n;
+	cin >> n;
+	vector<int> a;
+	a.resize(n + 1);
+	//a[n] = 0;
+	for (int i = 0; i < n; i++)
 	{
-		hash[str2[i]]++;
+		cin >> a[i];
 	}
-	for (int i = 0; i < str1.size(); i++)
+	int i = 0;
+	int count = 0;
+	while (i < n)
 	{
-		if (hash[str1[i]] == 0)
+		if (a[i] < a[i + 1])
 		{
-			ret += str1[i];
+			while (i < n && a[i] <= a[i + 1])
+			{
+				i++;
+			}
+			count++;
+			i++;
+		}
+		else if (a[i] == a[i + 1])
+		{
+			i++;
+		}
+		else
+		{
+			while (i < n && a[i] <= a[i + 1])
+			{
+				i++;
+			}
+			count++;
+			i++;
 		}
 	}
-	cout << ret << endl;
+	cout << count << endl; 
 	return 0;
 }
