cfcd0433a7325ee65d33ec0b3f8c511149b4acc2
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-27 11:04:31 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,37 +1,26 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class A
-{
-public:
-	A()
-	{
-		cout << "A::A()" << endl;
-	}
-	~A()
-	{
-		cout << "A::~A()" << endl;
-	}
-	int a = 0;
-};
-class B :public A
-{
-public:
-	B()
-	{
-		cout << "B::B()" << endl;
-	}
-	~B()
-	{
-		cout << "B::~B()" << endl;
-	}
-	int b = 0;
-};
-void f()
-{
-	B b;
-}
 int main()
 {
-	f();
+	string str1, str2;
+	//cin >> str1;
+	//cin >> str2;  这里不能用cin，cin遇到空格就退出了
+	getline(cin, str1);
+	getline(cin, str2);
+	int hash[256] = { 0 };
+	string ret = "";
+	for (int i = 0; i < str2.size(); i++)
+	{
+		hash[str2[i]]++;
+	}
+	for (int i = 0; i < str1.size(); i++)
+	{
+		if (hash[str1[i]] == 0)
+		{
+			ret += str1[i];
+		}
+	}
+	cout << ret << endl;
 	return 0;
 }
