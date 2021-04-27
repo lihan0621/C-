664fdcf3aa801c83f1a6e3c5ccd046b5a5b9303c
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-27 10:41:56 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,71 +1,23 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//class A
-//{
-//	friend long fun(A  s) {
-//		if (s.x < 3) {
-//			return 1;
-//		}
-//		return s.x + fun(A(s.x - 1));
-//	}
-//public:
-//	A(long a) {
-//		x = a--;
-//	}
-//private:
-//	long x;
-//};
-//int main()
-//{
-//	int sum = 0;
-//	for (int i = 0; i < 5; i++) {
-//		sum += fun(A(i));
-//	}
-//	cout << sum;
-//	return 0;
-//}
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-class parent
-{
-	int i;
-protected:
-	int x;
-public:
-	parent()
-	{
-		x = 0;
-		i = 0;
-	}
-	void change()
-	{
-		x++;
-		i++;
-	}
-	void display();
-};
-class son :public parent
-{
-public:
-	void modify();
-};
-void parent::display()
-{
-	cout << "x=" << x << endl;
-}
-void son::modify()
-{
-	x++;
-}
 int main()
 {
-	son A; parent B;
-	A.display();
-	A.change();
-	A.modify();
-	A.display();
-	B.change();
-	B.display();
+	int sum = 0;
+	int n = 0;
+	cin >> n;
+	vector<int> arr;
+	arr.resize(3 * n);
+	for (int i = 0; i < (3 * n); ++i)
+	{
+		cin >> arr[i];
+	}
+	std::sort(arr.begin(), arr.end());
+	for (int i = 0; i < n; ++i)
+	{
+		sum = sum + arr[arr.size() - (2 *  (i + 1))];
+	}
+	cout << sum << endl;
 	return 0;
 }
