1349ed34876899a7cc7114695a83352f60cfc089
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-28 10:05:53 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,44 +1,81 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	vector<int>array;
+//	array.push_back(100);
+//	array.push_back(300);
+//	array.push_back(300);
+//	array.push_back(300);
+//	array.push_back(300);
+//	array.push_back(500);
+//	vector<int>::iterator itor;
+//	for (itor = array.begin(); itor != array.end(); ++itor)
+//	{
+//		if (*itor == 300)
+//		{
+//			itor = array.erase(itor);
+//		}
+//	}
+//	for (itor = array.begin(); itor != array.end(); ++itor)
+//	{
+//		cout << *itor << " ";
+//	}
+//	return 0;
+//}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string a = "hello world";
+//	string b = a;
+//	if (a.c_str() == b.c_str())
+//	{
+//		cout << "true" << endl;
+//	}
+//	else cout << "false" << endl;
+//	string c = b;
+//	c = "";
+//	if (a.c_str() == b.c_str())
+//	{
+//		cout << "true" << endl;
+//	}
+//	else cout << "false" << endl;
+//	a = "";
+//	if (a.c_str() == b.c_str())
+//	{
+//		cout << "true" << endl;
+//	}
+//	else cout << "false" << endl;
+//	return 0;
+//}
 #include<iostream>
-#include<vector>
 using namespace std;
+class Sample
+{
+public:
+	Sample(int x)
+	{
+		p = new int(x);
+		//p = &x;
+		//*p = x;
+	}
+	~Sample()
+	{
+		if (p) delete p;
+	}
+	int show()
+	{
+		return *p;
+	}
+private:
+	int* p;
+};
 int main()
 {
-	int n;
-	cin >> n;
-	vector<int> a;
-	a.resize(n + 1);
-	//a[n] = 0;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	int i = 0;
-	int count = 0;
-	while (i < n)
-	{
-		if (a[i] < a[i + 1])
-		{
-			while (i < n && a[i] <= a[i + 1])
-			{
-				i++;
-			}
-			count++;
-			i++;
-		}
-		else if (a[i] == a[i + 1])
-		{
-			i++;
-		}
-		else
-		{
-			while (i < n && a[i] <= a[i + 1])
-			{
-				i++;
-			}
-			count++;
-			i++;
-		}
-	}
-	cout << count << endl; 
+	Sample s(5);
+	cout << s.show() << endl;
 	return 0;
 }
