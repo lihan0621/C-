018392eb604b1a82936c423d3b169c477b1eeac1
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-04-28 11:13:57 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,81 +1,15 @@
-//#include<iostream>
-//#include<vector>
-//using namespace std;
-//int main()
-//{
-//	vector<int>array;
-//	array.push_back(100);
-//	array.push_back(300);
-//	array.push_back(300);
-//	array.push_back(300);
-//	array.push_back(300);
-//	array.push_back(500);
-//	vector<int>::iterator itor;
-//	for (itor = array.begin(); itor != array.end(); ++itor)
-//	{
-//		if (*itor == 300)
-//		{
-//			itor = array.erase(itor);
-//		}
-//	}
-//	for (itor = array.begin(); itor != array.end(); ++itor)
-//	{
-//		cout << *itor << " ";
-//	}
-//	return 0;
-//}
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//int main()
-//{
-//	string a = "hello world";
-//	string b = a;
-//	if (a.c_str() == b.c_str())
-//	{
-//		cout << "true" << endl;
-//	}
-//	else cout << "false" << endl;
-//	string c = b;
-//	c = "";
-//	if (a.c_str() == b.c_str())
-//	{
-//		cout << "true" << endl;
-//	}
-//	else cout << "false" << endl;
-//	a = "";
-//	if (a.c_str() == b.c_str())
-//	{
-//		cout << "true" << endl;
-//	}
-//	else cout << "false" << endl;
-//	return 0;
-//}
 #include<iostream>
+#include<algorithm>
 using namespace std;
-class Sample
-{
-public:
-	Sample(int x)
-	{
-		p = new int(x);
-		//p = &x;
-		//*p = x;
-	}
-	~Sample()
-	{
-		if (p) delete p;
-	}
-	int show()
-	{
-		return *p;
-	}
-private:
-	int* p;
-};
 int main()
 {
-	Sample s(5);
-	cout << s.show() << endl;
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int A = (a + c) / 2;
+	int C = (b - d) / 2;
+	int B1 = (c - a) / 2;
+	int B2 = (b + d) / 2;
+	if (B1 != B2) cout << "NO";
+	else cout << A << " " << B1 << " " << C << endl;
 	return 0;
 }
