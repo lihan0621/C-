dcb074c7e2e839eb048e45fa73f6e8994d441b28
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-07 09:47:33 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,22 +1,58 @@
+//#include<iostream>
+//using namespace std;
+//int fun(int n)
+//{
+//	if (n < 2) {
+//		return n;
+//	}
+//	else {
+//		return 2 * fun(n - 1) + fun(n - 2);
+//	}
+//}
+//int main()
+//{
+//	cout << fun(5) << endl;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//struct A
+//{
+//	int a;
+//	short b;
+//	int c;
+//	char d;
+//};
+//struct B
+//{
+//	int a;
+//	short b;
+//	char c;
+//	int d;
+//};
+//int main()
+//{
+//	cout << sizeof(A) << sizeof(B) << endl;
+//	return 0;
+//}
 #include<iostream>
 using namespace std;
 int main()
 {
-	int f, f1 = 0, f2 = 1;
-	int N, left = 0, right = 0;
-	cin >> N;
-	while (1) {
-		f = f1 + f2;
-		f1 = f2;
-		f2 = f;
-		if (f < N) {
-			left = f;
-		}
-		else {
-			right = f;
+	int x = 3, y = 3;
+	switch (x % 2) {
+	case 1:
+		switch (y) {
+		case 0:
+			cout << "first";
+		case 1:
+			cout << "second";
 			break;
+		default:
+			cout << "hello";
 		}
+	case 2:
+		cout << "third";
 	}
-	cout << min(N - left, right - N) << endl;
 	return 0;
 }
