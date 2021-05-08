7d66c0efa1eba7595356e9fdcf7c496ae98c777a
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-09 00:48:22 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,58 +1,22 @@
-//#include<iostream>
-//using namespace std;
-//int fun(int n)
-//{
-//	if (n < 2) {
-//		return n;
-//	}
-//	else {
-//		return 2 * fun(n - 1) + fun(n - 2);
-//	}
-//}
-//int main()
-//{
-//	cout << fun(5) << endl;
-//	return 0;
-//}
-//#include<iostream>
-//using namespace std;
-//struct A
-//{
-//	int a;
-//	short b;
-//	int c;
-//	char d;
-//};
-//struct B
-//{
-//	int a;
-//	short b;
-//	char c;
-//	int d;
-//};
-//int main()
-//{
-//	cout << sizeof(A) << sizeof(B) << endl;
-//	return 0;
-//}
-#include<iostream>
+#include <iostream>
 using namespace std;
+class LCA
+{
+public:
+	int getLCA(int a, int b)
+	{
+		while (a != b) {
+			if (a > b) {
+				a = a / 2;
+			}
+			else if (a < b) {
+				b = b / 2;
+			}
+		}
+			return a;
+	}
+};
 int main()
 {
-	int x = 3, y = 3;
-	switch (x % 2) {
-	case 1:
-		switch (y) {
-		case 0:
-			cout << "first";
-		case 1:
-			cout << "second";
-			break;
-		default:
-			cout << "hello";
-		}
-	case 2:
-		cout << "third";
-	}
 	return 0;
 }
