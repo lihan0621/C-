4ef558b7dddea88b0982054dd663af3cda090986
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-12 10:34:49 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,24 +1,85 @@
-#include<iostream>
-using namespace std;
-class BinInsert
-{
-public:
-	int bininsert(int n, int m, int j, int i)
-	{
-		//方法一 :
-		//for (int k = 0; k <= i - j; k++) {
-		//	n |= ((m >> k) & 1) << (j + k);
-		//}
-		//cout << n << endl;
-		//return n;
-		//方法二 :
-		return n | (m << j);
-	}
-};
-int main()
-{
-	BinInsert a;
-	int ret = a.bininsert(1024, 19, 2, 6);
-	cout << ret << endl;
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//static int a = 1;
+//void fun1(void) { a = 2; }
+//void fun2(void) { int a = 3; }
+//void fun3(void) { static int a = 4; }
+//int main()
+//{
+//	cout << a << endl;
+//	fun1();	
+//	cout << a << endl;
+//	fun2();	
+//	cout << a << endl;
+//	fun3();
+//	cout << a << endl;
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//class A
+//{
+//public:
+//	void f()
+//	{
+//		cout << "A" << endl;
+//	}
+//};
+//class B : public A
+//{
+//public:
+//	virtual void f()
+//	{
+//		cout << "B" << endl;
+//	}
+//};
+//int main()
+//{
+//	A* a = new B;
+//	a->f();
+//	delete a;
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//class A
+//{
+//public:
+//	A()
+//	{
+//		cout << "A" << endl;
+//	}
+//	~A()
+//	{
+//		cout << "deA" << endl;
+//	}
+//};
+//class B
+//{
+//public:
+//	B()
+//	{
+//		cout << "B" << endl;
+//	}
+//	~B()
+//	{
+//		cout << "deB" << endl;
+//	}
+//};
+//class C :public A, public B
+//{
+//	C()
+//	{
+//		cout << "C" << endl;
+//	}
+//	~C()
+//	{
+//		cout << "deC" << endl;
+//	}
+//};
+//int main()
+//{
+//	A* a = new C;
+//	delete a;
+//	return 0;
+//}
