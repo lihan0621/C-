e23efcfb8384f432c4583184eb1c8b9d32c92ea6
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-14 00:00:53 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,34 +1,35 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include<iostream>
 using namespace std;
-void cmdLineParse(const string& str)
+//判断闰年
+bool isLeap(int year)
 {
-	string tmp = "";
-	vector<string> svec;
-	bool flag = false;//判断是否处于字符串状态
-	for (int i = 0; i < str.size(); ++i) {
-		if (str[i] == '"') {//判断是否是字符串的起始或者结束
-			flag = !flag;
-		}
-		else if (str[i] == ' ' && !flag) {//判断参数的分隔或者是否为字符串的内容
-			svec.push_back(tmp);
-			tmp = "";
-		}
-		else {//正常的参数内容
-			tmp += str[i];
-		}
-	}
-	svec.push_back(tmp);//追加最后一个参数
-	cout << svec.size() << endl;
-	for (int i = 0; i < svec.size(); ++i) {
-		cout << svec[i] << endl;
-	}
+	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
-//int main()
-//{
-//	string str;
-//	getline(cin, str);
-//	cmdLineParse(str);
-//	return 0;
-//}
+//根据年和月获取相应月份对应的天数
+int getDaysByYM(int year, int month)
+{
+	int days[13] = { 29,31,28, 31,30,31,30,31,31,30,31,30,31 };
+	if (month == 2 && isLeap(year)) {
+		month = 0;
+	}
+	return days[month];
+}
+//根据年月日获取相应的这一年的第几天
+int getDaysByYMD(int year, int month, int day)
+{
+	int days = 0;
+	for (int i = 1; i < month; ++i) {
+		days += getDaysByYM(year, i);
+	}
+	days += day;
+	return days;
+}
+int main()
+{
+	int year, month, day;
+	int days = 0;
+	cin >> year >> month >> day;
+	days = getDaysByYMD(year, month, day);
+	cout << days << endl;
+	return 0;
+}
