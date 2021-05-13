6b005515051325940b1dd4e5a653f75e4e7b5511
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-13 20:14:53 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,56 +1,45 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <iostream>
-//using namespace std;
-//int FindsubString(char* pch)
-//{
-//	int count = 0;
-//	char* p1 = pch;
-//	while (*p1 != '\0') {
-//		if (*p1 == p1[1] - 1) {
-//			p1++;
-//			count++;
-//		}
-//		else {
-//			break;
-//		}
-//	}
-//	int count2 = count;
-//	while (*p1 != '\0') {
-//		if (*p1 == p1[1] + 1) {
-//			p1++;
-//			count2--;
-//		}
-//		else {
-//			break;
-//		}
-//	}
-//	if (count2 == 0)
-//		return count;
-//	return 0;
-//}
-//void ModifyString(char* ptext)
-//{
-//	char* p1 = ptext;
-//	char* p2 = p1;
-//	while (*p1 != '\0') {
-//		int count = FindsubString(p1);
-//		if (count > 0) {
-//			*p2++ = *p1;
-//			sprintf(p2, "%i", count);
-//			while (*p2 != '\0') {
-//				p2++;
-//			}
-//			p1 += count + count + 1;
-//		}
-//		else {
-//			*p2++ = *p1++;
-//		}
-//	}
-//}
-//int main()
-//{
-//	char text[32] = "XYBCDCBABABA";
-//	ModifyString(text);
-//	printf(text);
-//	return 0;
-//}
+#include<iostream>
+#include<vector>
+#include<limits>
+#include<math.h>
+using namespace std;
+void get_div_num(int v, vector<int>& a)
+{
+	for (int i = 2; i <= sqrt(v); ++i) {
+		if (v % i == 0) {
+			a.push_back(i);
+			if (v / i != i)
+				a.push_back(v / i);
+		}
+	}
+}
+int Jump(int n, int m)
+{
+	vector<int> step(m + 1, INT_MAX);//int_max表示不可到达
+	step[n] = 0;//当前的位置初始化
+	for (int i = 0; i < m; ++i) {
+		if (step[i] == INT_MAX)
+			continue;
+		//获取i的约数并保存
+		vector<int> a;
+		get_div_num(i, a);
+		for (int j = 0; j < a.size(); ++j) {
+			if (a[j] + i <= m && step[a[j] + i] != INT_MAX) {
+				//需要挑选一个最小值
+				step[a[j] + i] = step[a[j] + i] < step[i] + 1 ? step[a[j] + i] : step[i] + 1;
+			}
+			else if (a[j] + i <= m) {
+				step[a[j] + i] = step[i] + 1;
+			}
+		}
+	}
+	return step[m] == INT_MAX ? -1 : step[m];
+}
+int main()
+{
+	int n, m, min_step;
+	cin >> n >> m;
+	min_step = Jump(n, m);
+	cout << min_step << endl;
+	return 0;
+}
