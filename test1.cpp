bb22b3b784026b82f02eb1daa7e7af88d099cbbe
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-13 20:14:32 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,85 +1,34 @@
-//#include<iostream>
-//using namespace std;
-//static int a = 1;
-//void fun1(void) { a = 2; }
-//void fun2(void) { int a = 3; }
-//void fun3(void) { static int a = 4; }
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+void cmdLineParse(const string& str)
+{
+	string tmp = "";
+	vector<string> svec;
+	bool flag = false;//判断是否处于字符串状态
+	for (int i = 0; i < str.size(); ++i) {
+		if (str[i] == '"') {//判断是否是字符串的起始或者结束
+			flag = !flag;
+		}
+		else if (str[i] == ' ' && !flag) {//判断参数的分隔或者是否为字符串的内容
+			svec.push_back(tmp);
+			tmp = "";
+		}
+		else {//正常的参数内容
+			tmp += str[i];
+		}
+	}
+	svec.push_back(tmp);//追加最后一个参数
+	cout << svec.size() << endl;
+	for (int i = 0; i < svec.size(); ++i) {
+		cout << svec[i] << endl;
+	}
+}
 //int main()
 //{
-//	cout << a << endl;
-//	fun1();	
-//	cout << a << endl;
-//	fun2();	
-//	cout << a << endl;
-//	fun3();
-//	cout << a << endl;
-//	return 0;
-//}
-//#include <iostream>
-//using namespace std;
-//class A
-//{
-//public:
-//	void f()
-//	{
-//		cout << "A" << endl;
-//	}
-//};
-//class B : public A
-//{
-//public:
-//	virtual void f()
-//	{
-//		cout << "B" << endl;
-//	}
-//};
-//int main()
-//{
-//	A* a = new B;
-//	a->f();
-//	delete a;
-//	return 0;
-//}
-//#include <iostream>
-//using namespace std;
-//class A
-//{
-//public:
-//	A()
-//	{
-//		cout << "A" << endl;
-//	}
-//	~A()
-//	{
-//		cout << "deA" << endl;
-//	}
-//};
-//class B
-//{
-//public:
-//	B()
-//	{
-//		cout << "B" << endl;
-//	}
-//	~B()
-//	{
-//		cout << "deB" << endl;
-//	}
-//};
-//class C :public A, public B
-//{
-//	C()
-//	{
-//		cout << "C" << endl;
-//	}
-//	~C()
-//	{
-//		cout << "deC" << endl;
-//	}
-//};
-//int main()
-//{
-//	A* a = new C;
-//	delete a;
+//	string str;
+//	getline(cin, str);
+//	cmdLineParse(str);
 //	return 0;
 //}
