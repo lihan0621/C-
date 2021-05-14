ca7271c21f3948288e7619e298c4041290bcfa3e
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-14 09:16:37 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,45 +1,41 @@
 #include<iostream>
 #include<vector>
-#include<limits>
-#include<math.h>
+#include<algorithm>
 using namespace std;
-void get_div_num(int v, vector<int>& a)
+int getLuckyPacket(vector<int>& x, int n, int pos, int sum, int multi)
 {
-	for (int i = 2; i <= sqrt(v); ++i) {
-		if (v % i == 0) {
-			a.push_back(i);
-			if (v / i != i)
-				a.push_back(v / i);
+	int count = 0;
+	for (int i = pos; i < n; ++i) {
+		sum += x[i];
+		multi *= x[i];
+		//判断幸运与否
+		if (sum > multi) {
+			count += 1 + getLuckyPacket(x, n, i + 1, sum, multi);
+		}
+		else if (x[i] == 1) {
+			count += getLuckyPacket(x, n, i + 1, sum, multi);
+		}
+		else {
+			break;//回溯
+		}
+		sum -= x[i];
+		multi /= x[i];
+		//去重
+		while (i < n - 1 && x[i] == x[i + 1]) {
+			i++;
 		}
 	}
-}
-int Jump(int n, int m)
-{
-	vector<int> step(m + 1, INT_MAX);//int_max表示不可到达
-	step[n] = 0;//当前的位置初始化
-	for (int i = 0; i < m; ++i) {
-		if (step[i] == INT_MAX)
-			continue;
-		//获取i的约数并保存
-		vector<int> a;
-		get_div_num(i, a);
-		for (int j = 0; j < a.size(); ++j) {
-			if (a[j] + i <= m && step[a[j] + i] != INT_MAX) {
-				//需要挑选一个最小值
-				step[a[j] + i] = step[a[j] + i] < step[i] + 1 ? step[a[j] + i] : step[i] + 1;
-			}
-			else if (a[j] + i <= m) {
-				step[a[j] + i] = step[i] + 1;
-			}
-		}
-	}
-	return step[m] == INT_MAX ? -1 : step[m];
+	return count;
 }
 int main()
 {
-	int n, m, min_step;
-	cin >> n >> m;
-	min_step = Jump(n, m);
-	cout << min_step << endl;
+	int n;
+	cin >> n;
+	vector<int> x(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> x[i];
+	}
+	sort(x.begin(), x.end());
+	cout << getLuckyPacket(x, n, 0, 0, 1) << endl;
 	return 0;
 }
