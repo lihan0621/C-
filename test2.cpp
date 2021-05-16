3bb873ba6ce862403ba893f89b12f7492bdc2525
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-16 21:25:21 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,35 +1,27 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-class Gloves
+int Count_Of_Perfect_Number(int n)
 {
-public:
-	int findMinimum(int n, vector<int> left, vector<int> right)
-	{
-		int sum = 0;
-		int left_sum = 0, left_min = INT_MAX;
-		int right_sum = 0, right_min = INT_MAX;
-		for (int i = 0; i < n; ++i) {
-			if (left[i] * right[i] == 0) {
-				sum += left[i] + right[i];
-			}
-			else {
-				left_sum += left[i];
-				left_min = left_min < left[i] ? left_min : left[i];
-				right_sum += right[i];
-				right_min = right_min < right[i] ? right_min : right[i];
+	int count = 0, sum = 0;
+	for (int i = 2; i < n; ++i) {
+		for (int j = 1; j < i; ++j) {
+			if (i % j == 0) {
+				sum += j;
 			}
 		}
-		return sum + min(left_sum - left_min + 1, right_sum - right_min + 1) + 1;
+		if (sum == i) {
+			count++;
+		}
+		sum = 0;
 	}
-};
+	return count;
+}
 int main()
 {
 	int n;
+	int count;
 	cin >> n;
-	vector<int> left = { 0, 7, 1, 6 };
-	vector<int> right = { 1, 5, 0, 6 };
-	Gloves a;
-	cout << a.findMinimum(n, left, right);
+	count = Count_Of_Perfect_Number(n);
+	cout << count << endl;
 	return 0;
 }
