fa9d2d3bc1fa4b297aed9d2f0b496abdc3d5c15f
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-17 23:12:41 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,13 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct student{
-	char name[1024];
-	int age;
 
-};
-int main()
-{
-	printf("hello bit!\n");
-	getchar();
-	return 0;
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+	int* nums3 = (int*)malloc(sizeof(int)*(m + n));
+	int i = 0, j = 0, idx = 0;
+	while (i<m&&j<n)
+	{
+		if (nums1[i] <= nums2[j])
+		{
+			nums3[idx++] = nums1[i++];
+		}
+		else
+		{
+			nums3[idx++] = nums2[j++];
+		}
+	}
+	if (i<m)
+		memcpy(nums3 + idx, nums1 + i, sizeof(int)*(m - i));
+	if (j<n)
+		memcpy(nums3 + idx, nums2 + j, sizeof(int)*(n - j));
+	memcpy(nums1, nums3, sizeof(int)*(m + n));
+	free(nums3);
 }
