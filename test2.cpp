9e16ed9bcf231e5b4889f6fc261a480ac984770c
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-19 00:35:17 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,39 +1,29 @@
 #include<iostream>
-#include<vector>
+#include<string>
 using namespace std;
-int FindIndex(int n)
+bool strMach(const char* pattern, const char* str)
 {
-	//先产生n行的类杨辉三角
-	int m = 2 * n - 1;//第n行具有的元素个数
-	vector<vector<int>> ivv(n, vector<int>(m, 0));
-	ivv[0][0] = 1;
-	for (int i = 0; i < n; ++i) {
-		ivv[i][0] = ivv[i][2 * i] = 1;//第一列和第二列
-		for (int j = 1; j < 2 * i; ++j) {
-			if (j == 1) {//第二列
-				ivv[i][j] = ivv[i - 1][j - 1] + ivv[i - 1][j];
-			}
-			else if (j == 2 * i - 1) {//倒数第二列
-				ivv[i][j] = ivv[i - 1][j - 2] + ivv[i - 1][j - 1];
-			}
-			else {
-				ivv[i][j] = ivv[i - 1][j - 2] + ivv[i - 1][j - 1] + ivv[i - 1][j];
-			}
-		}
-	}
-	//查询第n行的偶数所在的位置
-	for (int k = 0; k < m; ++k) {
-		if (ivv[n - 1][k] % 2 == 0) {
-			return k + 1;
-		}
-	}
-	return -1;
+	if (*pattern == '\0' && *str == '\0')
+		return true;
+	if (*pattern == '\0' || *str == '\0')
+		return false;
+	if (*pattern == '?')
+		return strMach(pattern + 1, str + 1);
+	else if (*pattern == '*')
+		return strMach(pattern + 1, str + 1) || strMach(pattern, str + 1) || strMach(pattern + 1, str);
+	else if (*pattern == *str)
+		return strMach(pattern + 1, str + 1);
+	else
+		return false;
 }
 int main()
 {
-	int n, index;
-	cin >> n;
-	index = FindIndex(n);
-	cout << index << endl;
+	string pattern, str;
+	cin >> pattern >> str;
+	bool ret = strMach(pattern.c_str(), str.c_str());
+	if (ret)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
 	return 0;
 }
