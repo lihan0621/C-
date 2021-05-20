879dd7cf246ca5740f22bf7bf1503c67e1413828
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-20 22:18:45 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,23 +1,45 @@
-#include<iostream>
-#include<string>
-using namespace std;
-void strReverse(string& str)
-{
-	int start = 0;
-	int end = str.size() - 1;
-	while (start < end) {
-		char tmp = str[start];
-		str[start] = str[end];
-		str[end] = tmp;
-		start++;
-		end--;
-	}
-}
+#include<stdio.h>
+#include<string.h>
 int main()
 {
-	string str;
-	cin >> str;
-	strReverse(str);
-	cout << str << endl;
+	//char a = -128;
+	//char a = 128;
+	//printf("%u\n", a);
+	//char会溢出, int也会溢出, 只要你这个类型是有上限的, 都会溢出
+	//int a = 10 * 10000 * 10000;
+	//int b = 20 * 10000 * 10000;
+	//int c = a + b;
+	//printf("%d\n", c);
+	//int i = -20;
+	//unsigned int j = 10;
+	//printf("%d\n", i + j);
+	//unsigned int i;
+	//for (i = 9; i >= 0; i--)
+	//{
+	//	printf("%u\n", i);
+	//}
+	//char a[1000];
+	//int i;
+	//for (i = 0; i < 1000; i++)
+	//{
+	//	a[i] = -1 - i;
+	//}
+	//printf("%d\n", strlen(a));
+	//unsigned char i = 0;
+	//for (i = 0; i <= 255; i++)
+	//{
+	//	printf("hello wolrd\n");
+	//}
+	//double a = 1.6;
+	//double b = 0.3;
+	//double c = a + b;
+	//double d = 1.9;
+	//if (c == d) {
+	//	printf("相等");
+	//}
+	//else {
+	//	printf("不相等");
+	//}
+
 	return 0;
 }
