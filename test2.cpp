4502ee6035c5f99d02f743411a14921a1c7f4725
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-21 16:47:57 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,45 +1,25 @@
-#include<stdio.h>
-#include<string.h>
+#include<iostream>
+#include<vector>
+using namespace std;
 int main()
 {
-	//char a = -128;
-	//char a = 128;
-	//printf("%u\n", a);
-	//char会溢出, int也会溢出, 只要你这个类型是有上限的, 都会溢出
-	//int a = 10 * 10000 * 10000;
-	//int b = 20 * 10000 * 10000;
-	//int c = a + b;
-	//printf("%d\n", c);
-	//int i = -20;
-	//unsigned int j = 10;
-	//printf("%d\n", i + j);
-	//unsigned int i;
-	//for (i = 9; i >= 0; i--)
-	//{
-	//	printf("%u\n", i);
-	//}
-	//char a[1000];
-	//int i;
-	//for (i = 0; i < 1000; i++)
-	//{
-	//	a[i] = -1 - i;
-	//}
-	//printf("%d\n", strlen(a));
-	//unsigned char i = 0;
-	//for (i = 0; i <= 255; i++)
-	//{
-	//	printf("hello wolrd\n");
-	//}
-	//double a = 1.6;
-	//double b = 0.3;
-	//double c = a + b;
-	//double d = 1.9;
-	//if (c == d) {
-	//	printf("相等");
-	//}
-	//else {
-	//	printf("不相等");
-	//}
-
+	int T, n, k;
+	cin >> T >> n >> k;
+	int num = 2 * n;
+	vector<int> card(num);
+	for (int i = 0; i < num; ++i)
+		cin >> card[i];
+	for (int i = 0; i < k; ++i) {
+		vector<int> tmp(card.begin(), card.end());
+		for (int j = 0; j < n; ++j) {
+			card[2 * j] = tmp[j];//左手的牌排放的位置
+			card[2 * j + 1] = tmp[j + n];//右手的牌排放的位置
+		}
+	}
+	//输出洗牌的顺序
+	for (int i = 0; i < num - 1; ++i) {
+		cout << card[i] << " ";
+	}
+	cout << card[num - 1] << endl;//最后一张牌后面不能有空格
 	return 0;
 }
