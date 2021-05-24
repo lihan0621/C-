63fe34ab7a4c796c7588ab68db74c23cfc1ef981
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-24 13:19:21 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,41 +1,31 @@
 #include<iostream>
-#include<string>
 #include<vector>
 using namespace std;
-int minDistance(const string& str1, const string& str2)
+class Bonus
 {
-	if (str1.empty() || str2.empty())
-		return max(str1.size(), str2.size());
-	int len1 = str1.size();
-	int len2 = str2.size();
-	vector<vector<int>> f(len1 + 1, vector<int>(len2 + 1, 0));
-	//初始化距离
-	for (int i = 0; i <= len1; ++i)
-		f[i][0] = i;
-	for (int j = 0; j <= len2; ++j)
-		f[0][j] = j;
-	for (int i = 1; i <= len1; ++i) {
-		for (int j = 1; j <= len2; ++j) {
-			if (str2[j - 1] == str1[i - 1]) {
-				f[i][j] = 1 + min(f[i - 1][j], f[i][j - 1]);
-				//由于字符相同,所以距离不发生变化
-				f[i][j] = min(f[i][j], f[i - 1][j - 1]);
+public:
+	int getMost(vector< vector<int>> board)
+	{
+		int row = board.size();
+		int col = board[0].size();
+		vector<vector<int>> allPrice(row, vector<int> (col, 0));
+		allPrice[0][0] = board[0][0];
+		for (int i = 0; i < row; ++i) {
+			for (int j = 0; j < col; ++j) {
+				if (i == 0 && j == 0)
+					continue;
+				if (i == 0)//在第一行只能往右走
+					allPrice[i][j] = allPrice[i][j - 1] + board[i][j];
+				else if (j == 0)
+					allPrice[i][j] = allPrice[i - 1][j] + board[i][j];
+				else
+					allPrice[i][j] = max(allPrice[i][j - 1], allPrice[i - 1][j] + board[i][j]);
 			}
-			else{
-				f[i][j] = 1 + min(f[i - 1][j], f[i][j - 1]);
-				//由于字符不相同,所以距离+1
-				f[i][j] = min(f[i][j], 1 + f[i - 1][j - 1]);
-			}
-			
 		}
+		return allPrice[row - 1][col - 1];
 	}
-	return f[len1][len2];
-}
+};
 int main()
 {
-	string str1, str2;
-	cin >> str1 >> str2;
-	int ret = minDistance(str1, str2);
-	cout << ret << endl;
 	return 0;
 }
