4f349fb5ead02a2dffbfee3726f7362a921d2ad2
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-24 12:37:36 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,25 +1,41 @@
 #include<iostream>
 #include<vector>
+#include <map>
+#include<algorithm>
 using namespace std;
+class Gift
+{
+public:
+	/*
+	int getValue(vector<int> gifts, int n)
+	{
+		map<int, int> val_count_map;
+		for (const auto& e : gifts)
+			val_count_map[e]++;
+		for (const auto& e : val_count_map) {
+			if (e.second > n / 2)
+				return e.first;
+		}
+		return 0;
+	}*/
+	int getValue(vector<int> gifts, int n)
+	{
+		sort(gifts.begin(), gifts.end());
+		int mid_val = gifts[n / 2];
+		int count = 0;
+		for (const auto& e : gifts) {
+			if (e == mid_val)
+				count++;
+		}
+		if (count > n / 2)
+			return mid_val;
+		return 0;
+	}
+};
 int main()
 {
-	int T, n, k;
-	cin >> T >> n >> k;
-	int num = 2 * n;
-	vector<int> card(num);
-	for (int i = 0; i < num; ++i)
-		cin >> card[i];
-	for (int i = 0; i < k; ++i) {
-		vector<int> tmp(card.begin(), card.end());
-		for (int j = 0; j < n; ++j) {
-			card[2 * j] = tmp[j];//左手的牌排放的位置
-			card[2 * j + 1] = tmp[j + n];//右手的牌排放的位置
-		}
-	}
-	//输出洗牌的顺序
-	for (int i = 0; i < num - 1; ++i) {
-		cout << card[i] << " ";
-	}
-	cout << card[num - 1] << endl;//最后一张牌后面不能有空格
+	Gift a;
+	vector<int> gifts = { 1,2,3,2,2 };
+	cout << a.getValue(gifts, 5) << endl;
 	return 0;
 }
