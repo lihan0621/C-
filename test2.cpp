3fc1703dc0b76ea14322eac23bf99032c98cfdac
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-24 14:15:43 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,41 +1,50 @@
 #include<iostream>
 #include<vector>
-#include <map>
-#include<algorithm>
 using namespace std;
-class Gift
+int ROW, COL;
+vector<vector<int>> maze;
+vector<vector<int>> path_tmp;//临时路径
+vector<vector<int>> path_best;//最佳路径
+void MazeTrack(int i, int j)
 {
-public:
-	/*
-	int getValue(vector<int> gifts, int n)
-	{
-		map<int, int> val_count_map;
-		for (const auto& e : gifts)
-			val_count_map[e]++;
-		for (const auto& e : val_count_map) {
-			if (e.second > n / 2)
-				return e.first;
-		}
-		return 0;
-	}*/
-	int getValue(vector<int> gifts, int n)
-	{
-		sort(gifts.begin(), gifts.end());
-		int mid_val = gifts[n / 2];
-		int count = 0;
-		for (const auto& e : gifts) {
-			if (e == mid_val)
-				count++;
-		}
-		if (count > n / 2)
-			return mid_val;
-		return 0;
+	maze[i][j] = 1;//代表i,j已经走过了
+	path_tmp.push_back({ i,j });
+	//判断是否到达出口
+	if (i == ROW - 1 && j == COL - 1) {
+		//寻找最短路径
+		if (path_best.empty() || path_best.size() > path_tmp.size())
+			path_best = path_tmp;
 	}
-};
+	//向右走
+	if (j + 1 < COL && maze[i][j + 1] == 0)
+		MazeTrack(i, j + 1);	
+	//向左走
+	if (j - 1 >= 0 && maze[i][j - 1] == 0)
+		MazeTrack(i, j - 1);
+	//向上走
+	if (i - 1 >= 0 && maze[i - 1][j] == 0)
+		MazeTrack(i - 1, j);
+	//向下走
+	if (i + 1 < ROW && maze[i + 1][j] == 0)
+		MazeTrack(i + 1, j);
+	maze[i][j] = 0;//回溯恢复路径
+	path_tmp.pop_back();
+}
 int main()
 {
-	Gift a;
-	vector<int> gifts = { 1,2,3,2,2 };
-	cout << a.getValue(gifts, 5) << endl;
+	cin >> ROW >> COL;
+	maze = vector<vector<int>>(ROW, vector<int>(COL, 0));//开辟迷宫空间
+	path_tmp.clear();
+	path_best.clear();
+	//首先输入迷宫
+	for (int i = 0; i < ROW; ++i) {
+		for (int j = 0; j < COL; ++j) {
+			cin >> maze[i][j];
+		}
+	}
+	MazeTrack(0, 0);//从起始点(0,0)开始走
+	for (int i = 0; i < path_best.size(); ++i) {
+		cout << "(" << path_best[i][0] << "," << path_best[i][1] << ")" << endl;
+	}
 	return 0;
 }
