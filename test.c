68d5e90e221cd77de9b2fa40c91680133316859d
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-25 18:32:32 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,13 +1,33 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct student{
-	char name[1024];
-	int age;
-
-};
-int main()
-{
-	printf("hello bit!\n");
-	getchar();
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 1, b = 0, c = -1, d = 0;
+//	d = ++a || ++b && ++c;
+//	printf("%d\n", d);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0, a = 0, b = 2, c = 3, d = 4;
+//	//i = a++ && ++b && d++;
+//	i = a++||++b||d++;
+//	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a = 1, b = 2, c = 3, d = 0;
+//	if (a == 1 && b++ == 2)
+//		if (b != 2 || c-- != 3)
+//			printf("%d,%d,%d\n", a, b, c);
+//		else
+//			printf("%d,%d,%d\n", a, b, c);
+//	else
+//		printf("%d,%d,%d\n", a, b, c);
+//
+//
+//	return 0;
+//}
