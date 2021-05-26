5add7db56b28b40c3b2df7f8f5832441ffa91df0
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-26 13:01:44 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,31 +1,20 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-class Bonus
-{
-public:
-	int getMost(vector< vector<int>> board)
-	{
-		int row = board.size();
-		int col = board[0].size();
-		vector<vector<int>> allPrice(row, vector<int> (col, 0));
-		allPrice[0][0] = board[0][0];
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				if (i == 0 && j == 0)
-					continue;
-				if (i == 0)//在第一行只能往右走
-					allPrice[i][j] = allPrice[i][j - 1] + board[i][j];
-				else if (j == 0)
-					allPrice[i][j] = allPrice[i - 1][j] + board[i][j];
-				else
-					allPrice[i][j] = max(allPrice[i][j - 1], allPrice[i - 1][j] + board[i][j]);
-			}
-		}
-		return allPrice[row - 1][col - 1];
-	}
-};
 int main()
 {
-	return 0;
+    vector<int> f_arr = { 0, 1, 2 };
+    for (int i = 3; i < 10001; ++i) {
+        f_arr.push_back((f_arr[i - 1] + f_arr[i - 2]) % 10000);
+    }
+    int n = 0;
+    while (cin >> n) {
+        for (int i = 0; i < n; ++i) {
+            int t;
+            cin >> t;
+            printf("%04d", f_arr[t]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
