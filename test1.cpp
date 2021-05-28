f6a7094c679b45b8955eccb551105df99e00bf29
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-28 08:11:42 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,29 +1,15 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	double n, a;
-//	cin >> n >> a;
-//	if (a >= (n * 2 * 3.14))
-//		cout << "Yes" << endl;
-//	else
-//		cout << "No" << endl;
-//	return 0;
-//}
 #include<iostream>
+#define ADD(x, y)  (x + y)
+#define CMP(x, y)  (x > y)
 using namespace std;
-int Jump(int num)
-{
-	if (num == 1)
-		return 1;
-	else
-		return 2 * Jump(num - 1);
-}
 int main()
 {
-	int num;
-	cin >> num;
-	int ret = Jump(num);
-	cout << ret << endl;
+	double a, b, c;
+	cin >> a >> b >> c;
+	//if (a + b > c && a + c > b && b + c > a)
+	if (CMP(ADD(a, b), c) && CMP(ADD(a, c), b) && CMP(ADD(b, c), a))
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
 	return 0;
 }
