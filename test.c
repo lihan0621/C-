d4eb8cbb0c1cceb2f1f38eb48dc9712189fa8c84
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-05-30 14:19:26 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,70 +1,74 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-int Add(int a, int b)
-{
-	return a + b;
-}
-int Sub(int a, int b)
-{
-	return a - b;
-}
-int Mul(int a, int b)
-{
-	return a * b;
-}
-int Div(int a, int b)
-{
-	return a / b;
-}
-int Mod(int a, int b)
-{
-	return a % b;
-}
-int main()
-{
-	int select = 1;
-	int op1, op2, result;
-	/// <summary>
-	system("title 简易计算器");
-	system("color 64");
-	/// </summary>
-	/// <returns></returns>
-	while (select) {
-		printf("***************************\n");
-		printf("*[1]Add       [2]Sub      *\n");
-		printf("*[3]Mul       [4]Div      *\n");
-		printf("*[5]Mod       [0]Quit     *\n");
-		printf("***************************\n");
-		printf("请选择:>");
-		scanf("%d", &select);
-		if (select == 0) 
-			break;
-		printf("请选择op1,op2:>");
-		scanf("%d,%d", &op1, &op2);
-		switch (select) {
-			case 1:
-				result = Add(op1, op2);
-				break;
-			case 2:
-				result = Sub(op1, op2);
-				break;
-			case 3:
-				result = Mul(op1, op2);
-				break;
-			case 4:
-				result = Div(op1, op2);
-				break;
-			case 5:
-				result = Mod(op1, op2);
-				break;
-			default:
-				printf("输入的cmd有误，请重新输入......\n");
-				break;
-		}
-		printf("result = %d\n", result);
-		system("cls");
-	}
-	printf("系统退出\n");
-	return 0;
-}
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+	//int a[] = { 1,2,3,4 };
+	//printf("%d\n", sizeof(a));
+	//printf("%d\n", sizeof(a + 0));
+	//printf("%d\n", sizeof(*a));
+	//printf("%d\n", sizeof(a + 1));
+	//printf("%d\n", sizeof(a[1]));
+	//printf("%d\n", sizeof(&a));
+	//printf("%d\n", sizeof(*&a));
+	//printf("%d\n", sizeof(&a + 1));
+	//printf("%d\n", sizeof(&a[0]));
+	//printf("%d\n", sizeof(&a[0] + 1));
+	//char arr[] = { 'a','b','c','d','e','f' };
+	//printf("%d\n", sizeof(arr));
+	//printf("%d\n", sizeof(arr + 0));
+	//printf("%d\n", sizeof(*arr));
+	//printf("%d\n", sizeof(arr[1]));
+	//printf("%d\n", sizeof(&arr));
+	//printf("%d\n", sizeof(&arr + 1));
+	//printf("%d\n", sizeof(&arr[0] + 1));
+	//printf("%d\n", strlen(arr));
+	//printf("%d\n", strlen(arr + 0));
+	//printf("%d\n", strlen(*arr));
+	//printf("%d\n", strlen(arr[1]));
+	//printf("%d\n", strlen(&arr));
+	//printf("%d\n", strlen(&arr + 1));
+	//printf("%d\n", strlen(&arr[0] + 1));
+	//char arr[] = "abcdef";
+	//printf("%d\n", sizeof(arr));
+	//printf("%d\n", sizeof(arr + 0));
+	//printf("%d\n", sizeof(*arr));
+	//printf("%d\n", sizeof(arr[1]));
+	//printf("%d\n", sizeof(&arr));
+	//printf("%d\n", sizeof(&arr + 1));
+	//printf("%d\n", sizeof(&arr[0] + 1));
+	//printf("%d\n", strlen(arr));
+	//printf("%d\n", strlen(arr + 0));
+	//printf("%d\n", strlen(*arr));
+	//printf("%d\n", strlen(arr[1]));
+	//printf("%d\n", strlen(&arr));
+	//printf("%d\n", strlen(&arr + 1));
+	//printf("%d\n", strlen(&arr[0] + 1));
+	//char* p = "abcdef";
+	//printf("%d\n", sizeof(p));
+	//printf("%d\n", sizeof(p + 1));
+	//printf("%d\n", sizeof(*p));
+	//printf("%d\n", sizeof(p[0]));
+	//printf("%d\n", sizeof(&p));
+	//printf("%d\n", sizeof(&p + 1));
+	//printf("%d\n", sizeof(&p[0] + 1));
+	//printf("%d\n", strlen(p));
+	//printf("%d\n", strlen(p + 1));
+	//printf("%d\n", strlen(*p));
+	//printf("%d\n", strlen(p[0]));
+	//printf("%d\n", strlen(&p));
+	//printf("%d\n", strlen(&p + 1));
+	//printf("%d\n", strlen(&p[0] + 1));
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(a[0][0]));
+//	printf("%d\n", sizeof(a[0]));
+//	printf("%d\n", sizeof(a[0] + 1));
+//	printf("%d\n", sizeof(*(a[0] + 1)));
+//	printf("%d\n", sizeof(a + 1));
+//	printf("%d\n", sizeof(*(a + 1)));
+//	printf("%d\n", sizeof(&a[0] + 1));
+//	printf("%d\n", sizeof(*(&a[0] + 1)));
+//	printf("%d\n", sizeof(*a));
+//	printf("%d\n", sizeof(a[3]));
+//	return 0;
+//}
