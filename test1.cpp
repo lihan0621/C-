42ae46dad48beb9f97c4c3a03f0048299a460f4d
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-07 09:13:06 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,28 +1,37 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <unordered_set>
 using namespace std;
 
 int main() {
-	int n;
-	while (cin >> n) {
-		getchar();
-		string name;
-		for (int i = 0; i < n; ++i) {
-			getline(cin, name);
-			if (name.find(',') == string::npos || name.find(' ') == string::npos) {
-				cout << "\"" << name << "\"";
+	string name;
+	unordered_set<string> s;
+	while (getline(cin, name)) {
+		int i = 0, end = 0;
+		while (i < name.size()) {
+			if (name[i] == '\"') {
+				end = name.find('"', i + 1);
+				s.insert(name.substr(i + 1, end - i - 1));
+				i = end + 2;
 			}
 			else {
-				cout << name;
-			}
-			if (i != n - 1) {
-				cout << ", ";
-			}
-			else {
-				cout << endl;
+				end = name.find(',', i + 1);
+				if (end == string::npos) {
+					s.insert(name.substr(i, name.size()));
+					break;
+				}
+				s.insert(name.substr(i, end - i));
+				i = end + 1;
 			}
 		}
+		getline(cin, name);
+		if (s.find(name) == s.end()) {
+			cout << "Important" << endl;
+		}
+		else {
+			cout << "Ignore" << endl;
+		}
 	}
-
 	return 0;
 }
+
