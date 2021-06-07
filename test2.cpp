a6959662a515d9d0b17b7c90a155e1f99d88fe43
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-07 10:09:03 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+	long long d[21] = { 0 ,0 ,1 };
+	long long f[21] = { 1, 1, 2 };
+	for (int i = 3; i <= 20; i++) {
+		d[i] = (i - 1) * (d[i] - 1 + d[i - 2]);
+		f[i] = f[i - 1] * i;
+	}
 	int n;
 	while (cin >> n) {
-		long long f0 = 1;
-		long long f1 = 1;
-		long long ret = 0;
-		for (int i = 2; i <= n; i++) {
-			ret = f0 + f1;
-			f0 = f1;
-			f1 = ret;
-		}
-		cout << ret << endl;
+		printf("%.2f%%\n", 100.0 * d[n] / f[n]);
 	}
 	return 0;
 }
