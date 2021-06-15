aaa93684a323eb0374c656dd4a51edb31f051539
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-15 18:13:02 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,59 +1,30 @@
-#include <stdio.h>
-#include <assert.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace std;
 
-const char* myStrstr(const char* str1, const char* str2) {
-	assert(str1 != NULL);
-	assert(str2 != NULL);
-	assert(*str1 != '\0');
-	assert(*str2 != '\0');
-	/*if (*str1 == '\0')
-		return "";*/
-	const char* black = str1;
-	while (*black != '\0') {
-		const char* red = black;
-		const char* sub = str2;
-		while (*red != '\0' && *sub != '\0' && *red == *sub) {
-			red++;
-			sub++;
-		}
-		//if (*red == '\0' || *red == *sub)
-		//	continue;
-		if(*sub == '\0')
-			return black;
-		black++;
-	}
-	return NULL;
-	
-}
-
-void* myMemcpy(void* dest, const void* src, size_t num) {
-	assert(dest != NULL);
-	assert(src != NULL);
-	assert(num != NULL);
-	char* cdest = (char*)dest;
-	const char* csrc = (const char*)src;
-	for (size_t i = 0; i < num; i++) {
-		cdest[i] = csrc[i];
-	}
-	return dest;
-}
 int main() {
-
-	//int arr[] = { 1,2,3,4 };
-	//int arr2[100] = { 0 };
-	//myMemcpy(arr2, arr, sizeof(arr));
-	//for (int i = 0; i < 4; i++) {
-	//	printf("%d\n", arr2[i]);
-	//}
-
-	//int a = 10;
-	//int b = 0;
-	//myMemcpy(&a, &b, sizeof(a));
-	//printf("b = %d", a);
-
-	//const char* str1 = "hello world";
-	//const char* str2 = "llo";
-	//const char* ret = myStrstr(str1, str2);
-	//printf("%s", ret);
+	string s1, s2;
+	int len1, len2;
+	while (cin >> s1 >> s2 >> len1 >> len2) {
+		//将s1和s2的长度补充到len2
+		//注意: s1之后补'a', s2之后补'z'+1
+		s1.append('a', len2 - s1.size());
+		s2.append('z' + 1, len2 - s2.size());
+		//使用s2和s1对应位置上的字符相减并保存
+		vector<int> v(len2);
+		for (int i = 0; i < len2; ++i) {
+			v[i] = s2[i] - s1[i];
+		}
+		//计算结果
+		long long result = 0;
+		for (int i = len1; i <= len2; ++i) {
+			for (int j = 0; j < i; ++j) {
+				result += v[j] * pow(26, i - j - 1);
+			}
+		}
+		cout << (result - 1) % 1000007 << endl;
+	}
 	return 0;
 }
