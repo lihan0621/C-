0055d2671f9bfccfdc2a3f4c553cd35c79cecb07
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-15 18:34:52 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,89 +1,62 @@
-#include<iostream>
-#include<vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
 using namespace std;
-template<class T>
-class PriorityQueue
-{
-public:
-	void shiftUp(int child)
-	{
-		int parent = (child - 1) / 2;
-		//调整
-		while (child > 0)
-		{
-			if (v[parent] < v[child])
-			{
-				swap(v[parent], v[child]);
-				child = parent;
-				parent = (child - 1) / 2;
-			}
-			else
-				break;
-		}
-	}
-	void shiftDown()
-	{
-		int parent = 0;
-		int child = 2 * parent + 1;
-		while (child < v.size())
-		{
-			//从左右孩子中找到最大值
-			if (child + 1 < v.size() && v[child] < v[child + 1])
-				++child;
-			if (v[parent] < v[child])
-			{
-				swap(v[parent], v[child]);
-				parent = child;
-				child = 2 * parent + 1;
-			}
-			else
-				break;
-		}
-	}
-	void push(const T& val)
-	{
-		v.push_back(val);
-		shiftUp(v.size() - 1);
-	}
-	void pop()
-	{
-		swap(v[0], v[v.size() - 1]);
-		v.pop_back();
-		shiftDown();
-	}
-	T& top()
-	{
-		return v[0];
-		//return v.front();
-	}
-	size_t size() const
-	{
-		return v.size();
-	}
-	bool empty() const
-	{
-		return v.empty();
-	}
-private:
-	vector<T> v;
+#define DIGROW 5
+#define DIGCOL 5
+#define VIEWNUM 65
+typedef char Array[DIGROW][DIGCOL];
+static const int dig[10][20] = {
+	{ 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1 },
+	{ 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+	{ 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1 },
+	{ 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1 },
+	{ 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0 },
+	{ 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1 },
+	{ 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 },
+	{ 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+	{ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 },
+	{ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1 }
 };
-void test()
+void InitArray(Array ar)
 {
-	PriorityQueue<int> q;
-	q.push(100);
-	q.push(1);
-	q.push(150);
-	q.push(10);
-	q.push(20);
-	while (!q.empty())
+	memset(ar, ' ', sizeof(Array));
+	for (int i = 0; i < DIGROW; ++i)
 	{
-		cout << q.top() << " ";
-		q.pop();
+		ar[i][DIGCOL - 1] = '\0';
 	}
-	cout << endl;
+}
+void ShowArray(Array ar,int num)
+{
+	InitArray(ar);
+	for (int i = 0; i < 20; ++i)
+	{
+		if (dig[num][i] == 1)
+		{
+			ar[i / (DIGCOL-1)][i % (DIGCOL-1)] = VIEWNUM;
+		}
+	}
+	system("cls");
+	for (int i = 0; i < DIGROW; ++i)
+	{
+		for (int j = 0; j < DIGCOL - 1; ++j)
+		{
+			printf("%c", ar[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 int main()
 {
-	test();
+	Array ar;
+	int num;
+	do
+	{
+		scanf_s("%d", &num);
+		ShowArray(ar, num);
+	} while (1);
+	system("pause");
 	return 0;
 }
