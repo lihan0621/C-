e08fec0f3d4bc3320893acddbd65fc520dc87e8a
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-20 12:52:49 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+bool isBrother(string res, string s) {
+	if (res == s) {
+		return false;
+	}
+	sort(res.begin(), res.end());
+	sort(s.begin(), s.end());
+	if (res == s) {
+		return true;
+	}
+	return false;
+}
 int main() {
-	int m, n, k;
-	while (cin >> m >> n >> k) {
-		// 因为地图大小已经确定好了，map直接设置好大小
-		vector<vector<int>> map(n + 1, vector<int>(m + 1));
-		// 向地图中放入蘑菇
-		int row, col;
-		while (k--){
-			cin >> row >> col;
-			map[row][col] = 1;
+	int num;
+	while (cin >> num) {
+		string str;
+		string word, s;
+		vector<string> vs;
+		int index;
+		for (int i = 0; i < num; ++i) {
+			cin >> str;
+			vs.push_back(str);
 		}
-		vector<vector<double>> dp(n + 1, vector<double>(m + 1));
-		dp[1][1] = 1.0;
-		for (int i = 1; i <= n; i++){
-			for (int j = 1; j <= m; ++j){
-				// 对于每个位置，按照上述转移方程来确定概率
-				if (!(i == 1 && j == 1))
-					dp[i][j] = dp[i - 1][j] * (j == m ? 1 : 0.5) + dp[i][j - 1] * (i == n ? 1 : 0.5);
-				// 如果该位置为蘑菇，表示不能到达该位置，则到达该位置的概率一定为0
-				if (map[i][j])
-					dp[i][j] = 0;
+		sort(vs.begin(), vs.end());
+		cin >> word;
+		cin >> index;
+		int count = 0;
+		for (int i = 0; i < num; ++i) {
+			if (isBrother(word, vs[i])) {
+				count++;
+			}
+			if (count == index) {
+				s = vs[i];
 			}
 		}
-		printf("%.2f\n", dp[n][m]);
+		if (!vs.empty()) {
+			cout << count << endl;
+		}
+		if (count >= index) {
+			cout << s << endl;
+		}
 	}
 	return 0;
 }
