fd23d43fcba013c9a35fe5f7b9304236b06860a9
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-26 09:42:26 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,143 +1,202 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define A 4 + 5
-#define B A*A
-// N 2
-//#define M N+1
-//#define NUM (M+1)*M/2
-//int func(int a)
-//{
-//	int b;
-//	switch (a)
-//	{
-//	case 1: b = 30;
-//	case 2: b = 20;
-//	case 3: b = 16;
-//	default: b = 0;
-//	} 
-//	return b;
-//}
-//int f(int n)
-//{
-//	if (n) return f(n - 1) + n;
-//	else return n;
-//}
-//int a, b;
-//void fun()
-//{
-//	a = 100;
-//	b = 200;
-//}
-//struct HAR{ int x, y; struct HAR *p; } h[2];
-//int func(int i, int j)
-//	{
-//			if (i <= 0 || j <= 0)
-//				return 1;
-//			return 2 * func(i - 3, j / 2);
-//		}
-//int main()
-//{
-	//h[0].x = 1; h[0].y = 2;
-	//h[1].x = 3; h[1].y = 4;
-	//h[0].p = &h[1]; h[1].p = h;
-	//printf("%d,%d \n", (h[0].p)->x, (h[1].p)->y);
-	//int a, b;
-	//scanf("%d,%d", &a, &b);
-	//if (a>b) a = b; b = a;
-	//else a++; 
-	//b++;
-	//	
-	//char s[] = "abcdefgh", *p = s;
-	//p += 3;
-	//printf("%d\n", strlen
-	//	(strcpy(p, "ABCD")));
-	/*printf("%d\n", func(15,20));*/
-	//int a[] = { 2, 4, 6, 8 }, *p = a, i;
-	//for (i = 0; i<4; i++)
-	//	a[i] = *p++;
-	//printf("%d\n", a[2]);
-	//int a = 1; int b = 2;
-	//for (; a<8; a++)
-	//{
-	//	b += a; a += 2;
-	//}
-	//printf("%d\n", B*B);
-	//short i = 65537;
-	//int j = i + 1;
-	//printf("i=%d,j=%d\n", i, j);
-//	int a[] = { 2, 4, 6, 8, 10 }, *p, **k;
-//	p = a;
-//	k = &p;
-//	printf(" % d", *(p++));
-//	printf(" % d\n", **k);
-//		getchar();
-//	return 0;
-//}
-//int main()
-//{
-//	int a = 5, b = 7;
-//	fun();
-//	printf("%d %d\n", a, b);
-//int main()
-//{
-//	printf("%d", f(4));
-//	//char str[] = "ABCD", *p = str;
-//	//printf("%d\n", *(p + 4));
-//	//char *p = "abcdefgh", *r;
-//	//long *q;
-//	//q = (long*)p;
-//	//q++;
-//	//r = (char*)q;
-//	//printf("%s\n", r);
-//
-//	/*func(1);
-//	int b;*/
-//	//int n = 2;
-//	//n += n -= n*n;
-//	//printf("%d\n", n);
-//
-////int main()
-////{
-////		int i = 0, a = 0;
-////		while (i<20)
-////		{
-////			for (;;)
-////			{
-////				if ((i % 10) == 0) break;
-////				else i--;
-////			} i
-////				+= 11; a += i;
-////		}
-////		printf("%d\n", a);
-//	getchar();
-//	return 0;
-//}
-//#include <iostream>
-//using namespace std;
-//int fun(int x, int y)
-//{
-//	if (x == y)
-//		return (x);
-//	else
-//		return ((x + y) / 2);
-//} 
-//int main()
-//{
-//	int a = 4, b = 5, c = 6;
-//	printf("%d\n", fun(2 * a, fun(b, c)));
-//	getchar();
-//	return 0;
-//}
-int* Array()
+#include<iostream>
+using namespace std;
+class Date
 {
-	int arr[] = { 1, 2, 3 };
-	return arr;
-}
-int main()
-{
+public:
+	Date(int y = 1, int m = 1, int d = 1)
+	{
+		//判断日期是否有效
+		if (y <= 0 || m <= 0 || m > 12 || d <= 0 || d > getDay(y, m))
+		{
+			_y = 1;
+			_m = 1;
+			_d = 1;
+			cout << "日期无效,默认为1-1-1" << endl;
+		}
+		else
+		{		
+			_y = m;
+			_m = m;
+			_d = d;
+		}
 
-	getchar();
-	return 0;
-}
+	}
+	int getDay(int y, int m)
+	{
+		static int days[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int day = days[m];
+		//如果是二月,且为闰年,修改天数
+		if ((m == 2) && ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)))
+		{
+			day += 1;
+			return day;
+		}
+	}
+	//日期运算
+	//Date + int    int a,int b,a += b;
+	//左操作数 += 右操作数 :左操作数的内容发生变化,返回值:相加之后的值
+	Date& operator+=(int day)
+	{
+		//判断日期是否为负
+		if (day<0)
+		{
+			return *this -= -day;
+		}
+		//相加天数
+		_d += day;
+		//判断天数是否溢出
+		while (_d > getDay(_y, _m))
+		{
+			//减去当月的天数
+			_d -= getDay(_y, _m);
+			//月份进位
+			++_m;
+			//判断月份是否溢出
+			if (_m == 13)
+			{
+				//年份进位
+				++_y;
+				_m = 1;
+			}
+		}
+		//返回相加之后的结果
+		return *this;
+	}
+	//返回相加之后的结果
+	//Date + int
+	Date operator+(int day)
+	{
+		Date copy(*this);
+		//Data copy = *this
+		copy += day;
+		return copy;
+	}
+	//前置++
+	//++Date
+	Date& operator++()
+	{
+		return *this += 1;
+	}
+	//后置++
+	//Date++
+	//int参数的作用:只是为了和前置++构成重载,函数内部不会用到
+	Date operator++(int)
+	{
+		Date copy(*this);
+		*this += 1;
+		return copy;
+	}
+	Date& operator-=(int day)
+	{
+		_d -= day;
+		while (_d <= 0)
+		{
+			//用上一月的天数回补
+			--_m;
+			//判断月份是否溢出(向下)
+			if (_m == 0)
+			{
+				//需要退回上一年的12月份
+				--_y;
+				_m = 12;
+			}
+			_d += getDay(_y, _m);
+		}
+		return *this;
+	}
+	//Date - int
+	Date operator-(int day)
+	{
+		Data copy(*this);
+		copy -= day;
+		return copy;
+	}
+	//--Date
+	Data operator--()
+	{
+		return *this -= 1;
+	}
+	//Date--
+	Date& operator--(int)
+	{
+		Date copy(*this);
+		*this -= 1;
+		return copy;
+	}
+	//==
+	bool operator==(const Date& d)
+	{
+		return _y == d._y
+			&& _m == d._m
+			&& _d == d._d;
+	}
+	//!=
+	bool operator!=(const Date& d)
+	{
+		return !(*this == d);
+	}
+	//>
+	bool operator>(const Date& d)
+	{
+		if (_y > d._y)
+		{
+			return true;
+		}
+		else if (_y == d.y)
+		{
+			if (_m > d._m)
+			{
+				return true;
+			}
+			else if (_m == d._m)
+			{
+				if (_d > d._d)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	//<
+	bool operator>(const Date& d)
+	{
+		return !(*this >= d);
+	}
+	//>=
+	bool operator>=(const Date& d)
+	{
+		return *this > d || *this == d;
+	}
+	//<=
+	bool operator<=(const Date& d)
+	{
+		return !(*this > d);
+	}
+	//日期相减:Date - Data
+	int operator-(const Date& d)
+	{
+		//计算比较小的日期经过多少次的自加运算可以和较大日期相等
+		//自加的天数就是两个日期相差的天数
+		Date max = *this;
+		Date min = d;
+		int flag = 1;
+		if (d > *this)
+		{
+			max = d;
+			min = *this;
+			flag = -1;
+		}
+		int day = 0;
+		while (min < max)
+		{
+			++min;
+			++day;
+		}
+		return flag * day;
+	}
+private:
+	int _y;
+	int _m;
+	int _d;
+};
