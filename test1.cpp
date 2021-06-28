8f97fdf7c2d529d32161aa5677906235265994a8
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-28 09:10:48 +0800

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,75 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <limits.h>
-using namespace std;
+#include <stdio.h>
+#include <assert.h>
 
-long long NK_zheng[51][11];
-long long NK_fu[51][11];
+const char* myStrstr(const char* str1, const char* str2) {
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	assert(*str1 != '\0');
+	assert(*str2 != '\0');
+	/*if (*str1 == '\0')
+		return "";*/
+	const char* black = str1;
+	while (*black != '\0') {
+		const char* red = black;
+		const char* sub = str2;
+		while (*red != '\0' && *sub != '\0' && *red == *sub) {
+			red++;
+			sub++;
+		}
+		//if (*red == '\0' || *red == *sub)
+		//	continue;
+		if(*sub == '\0')
+			return black;
+		black++;
+	}
+	return NULL;
+	
+}
+
+void* myMemcpy(void* dest, const void* src, size_t num) {
+	assert(dest != NULL);
+	assert(src != NULL);
+	assert(num != NULL);
+	char* cdest = (char*)dest;
+	const char* csrc = (const char*)src;
+	for (size_t i = 0; i < num; i++) {
+		cdest[i] = csrc[i];
+	}
+	return dest;
+}
 int main() {
-	int N;
-	cin >> N;
-	vector<int> V(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		cin >> V[i];
-	}
-	int K, D;
-	cin >> K >> D;
-	for (int n = 1; n <= 50; ++n) {
-		for (int k = 1; k <= 10; ++k) {
-			NK_zheng[n][k] = LLONG_MIN;
-			NK_fu[n][k] = LLONG_MAX;
-		}
-	}
-	for (int n = 1; n <= N; ++n) {
-		if (V[n] >= 0) {
-			NK_zheng[n][1] = V[n];
-		}
-		else {
-			NK_fu[n][1] = V[n];
-		}
-	}
-	for (int n = 2; n <= N; ++n) {
-		for (int k = 2; k <= K && k <= n; ++k) {
-			long long max = LLONG_MIN;
-			long long min = LLONG_MAX;
-			for (int temp = (k - 1 > n - D) ? (k - 1) : (n - D); temp <= n - 1; ++temp) {
-				if (max < NK_zheng[temp][k - 1]) {
-					max = NK_zheng[temp][k - 1];
-				}
-				if (min < NK_fu[temp][k - 1]) {
-					min = NK_fu[temp][k - 1];
-				}
-			}
-			if (max != LLONG_MIN) {
-				if (V[n] >= 0) {
-					NK_zheng[n][k] = max * V[n];
-				}
-				else {
-					NK_fu[n][k] = max * V[n];
-				}
-			}
-			if (min != LLONG_MAX) {
-				if (V[n] < 0 && NK_zheng[n][k] < min * V[n]) {
-					NK_zheng[n][k] = min * V[n];
-				}
-				else if (NK_fu[n][k] > min * V[n]) {
-					NK_fu[n][k] = min * V[n];
-				}
-			}
-			long long max1 = LLONG_MIN;
-			long long max2 = LLONG_MIN;
-			for (int n = k; n <= N; ++n) {
-				if (max1 < NK_zheng[n][k]) {
-					max1 = NK_zheng[n][k];
-				}
-				if (max2 < NK_fu[n][k] && NK_fu[n][k] != LLONG_MAX) {
-					max2 = NK_fu[n][k];
-				}
-			}
-			max1 = max1 > max2 ? max1 : max2;
-			cout << max1 << endl;
-		}
-	}
+
+	//int arr[] = { 1,2,3,4 };
+	//int arr2[100] = { 0 };
+	//myMemcpy(arr2, arr, sizeof(arr));
+	//for (int i = 0; i < 4; i++) {
+	//	printf("%d\n", arr2[i]);
+	//}
+
+	//int a = 10;
+	//int b = 0;
+	//myMemcpy(&a, &b, sizeof(a));
+	//printf("b = %d", a);
+
+	//const char* str1 = "hello world";
+	//const char* str2 = "llo";
+	//const char* ret = myStrstr(str1, str2);
+	//printf("%s", ret);
 	return 0;
 }
