fbb554418fb893c98774b0b7151d971f0eb6c0cf
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-06-28 09:10:00 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,126 +1,47 @@
-#define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-typedef struct Person{
-	char name[1024];
-	char tel[1024];
-}Person;
-#define MAX_PERSON 1024
-typedef struct AddressBook{
-	Person persons[MAX_PERSON];
-	int size;
-}AddressBook;
-int menu(){
-	printf("========================\n");
-	printf("===1.新增联系人=========\n");
-	printf("===2.查找联系人=========\n");
-	printf("===3.删除联系人=========\n");
-	printf("===4.修改联系人=========\n");
-	printf("===5.查看所有联系人=====\n");
-	printf("===6.清空所有联系人=====\n");
-	printf("===0.退出===============\n");
-	printf("========================\n");
-	printf("请输入您的选择:");
-	int choice = 0;
-	scanf("%d",&choice);
-	return choice;
-}
-void init(AddressBook* addressBook){
-	addressBook->size = 0;
-}
-void addPerson(AddressBook* addressBook){
-	printf("新增联系人\n");
-	int currentsize = addressBook->size;
-	if (currentsize >= MAX_PERSON){
-		printf("通讯录已满,新增失败\n");
-		return;
-	}
-	Person* p = &addressBook->persons[addressBook->size];
-	printf("请输入新联系人的姓名: ");
-	scanf("%s", p->name);
-	printf("请输入新联系人的电话: ");
-	scanf("%s",p->tel);
-	printf("新增联系人成功\n");
-	addressBook->size++;
-}
-void findPerson(AddressBook* addressBook){
-	print("按照姓名查找联系人\n");
-	printf("请输入要查找的联系人\n");
-	char name[1024] = { 0 };
-	scanf("%s", name);
-	for (int i = 0; i < addressBook->size; i++){
-		Person* p = &addressBook->persons[i];
-		if (strcmp(name, p->name) == 0){
-			printf("[%d]\t%s\t%s\n",i,p->name,p->tel);
-		}
-	}
-	printf("查找联系人完成\n");
-}
-void delPerson(AddressBook* addressBook){
-	printf("删除联系人\n");
-	printf("请输入要删除的联系人的编号: ");
-	int id = 0;
-	scanf("%d",&id);
-	if (id < 0 || id >= addressBook->size){
-		printf("您的输入有误\n");
-		return;
-	}
-	if (id == addressBook->size){
-		addressBook->size--;
-		printf("删除成功\n");
-		return;
-	}
-	else{
-		addressBook->persons[id] = addressBook->persons[addressBook->size - 1];
-		addressBook->size--;
-		printf("删除成功\n");
-	}
-}
-void updatePerson(AddressBook* addressBook){
-}
-void printPerson(AddressBook* addressBook){
-	printf("打印所有联系人\n");
-	for (int i = 0; i < addressBook->size; i++){
-		Person* p = &addressBook->persons[i];
-		printf("[%d]\t%s\t%s\n",i,p->name,p->tel);
-	}
-	printf("共计 %d 条记录\n", addressBook->size);
-}
-void clearPerson(AddressBook* addressBook){
-	printf("确认您要清空吗?(Y/N)\n");
-	char Choice = 0;
-	scanf("%s",&Choice);
-	if (Choice == 'Y'){
-		addressBook->size = 0;
-	}
-	return;
-}
-typedef void(*Func)(AddressBook*);
-AddressBook addressBook;
-int main(){
-	Func funcs[] = {
-		NULL,
-		addPerson,
-		findPerson,
-		delPerson,
-		updatePerson,
-		printPerson,
-		clearPerson
-	};
-	init(&addressBook);
-	while (1){
-		int choice = menu();
-		if (choice < 0 || choice >= sizeof(funcs) / sizeof(funcs[0])){
-			printf("您的输入有误\n");
-			continue;
-		}
-		if (choice == 0){
-			printf("goodbye\n");
-			break;
-		}
-		funcs[choice](&addressBook);
-	}
-	system("pause");
+struct Test
+{
+	int Num;
+	char* pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p = NULL;
+int main()
+{
+	//int* p = NULL;
+	//printf("%p\n", p);
+	//printf("%x\n", p);
+	//printf("%p\n", p + 0x1);
+	//printf("%p\n", (unsigned long)p + 0x1);
+	//printf("%p\n", (unsigned int*)p + 0x1);
+	//int a[4] = { 1, 2, 3, 4 };
+	//int* ptr1 = (int*)(&a + 1);
+	//int* ptr2 = (int*)((int)a + 1);
+	//printf("%x,%x", ptr1[-1], *ptr2);
+	//int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+	//int* p;
+	//p = a[0];
+	//printf("%d", p[0]);
+	//int a[5][5];
+	//int(*p)[4];
+	//p = a;
+	////printf("a_ptr=%#p,p_ptr=%#p\n", &a[4][2], &p[4][2]);
+	//printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+	//int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	//int* ptr1 = (int*)(&aa + 1);
+	//int* ptr2 = (int*)(*(aa + 1));
+	//printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+	//char* a[] = { "work","at","alibaba" };
+	//char** pa = a;
+	//pa++;
+	//printf("%s\n", *pa);
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
 	return 0;
 }
