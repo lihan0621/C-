1ad7edc7ddd517189c7e5d033c0a04234fe67bf1
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-04 15:09:07 +0800

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,28 +1,58 @@
-//#include<stdio.h>
-//#include<string.h>
-////void func(){}
-////int add(int x, int y) {
-////	return x + y;
-////}
-////int sub(int x, int y) {
-////	return x - y;
-////}
-////int mul(int x, int y) {
-////	return x * y;
-////}
-//
+//#include<iostream>
+//using namespace std;
+//int fun(int n)
+//{
+//	if (n < 2) {
+//		return n;
+//	}
+//	else {
+//		return 2 * fun(n - 1) + fun(n - 2);
+//	}
+//}
 //int main()
 //{
-//	//void(*p)() = func;
-//	//int(*p2)(int, int) = add;
-//	////初始的空瓶数
-//	//int bottle = 20;
-//	////喝到的总的瓶数
-//	//int total = 20;
-//	//while (bottle > 1) {
-//	//	total += bottle / 2;
-//	//	bottle = bottle / 2 + bottle % 2;
-//	//}
-//	//printf("tolal = %d\n", total);
+//	cout << fun(5) << endl;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//struct A
+//{
+//	int a;
+//	short b;
+//	int c;
+//	char d;
+//};
+//struct B
+//{
+//	int a;
+//	short b;
+//	char c;
+//	int d;
+//};
+//int main()
+//{
+//	cout << sizeof(A) << sizeof(B) << endl;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int x = 3, y = 3;
+//	switch (x % 2) {
+//	case 1:
+//		switch (y) {
+//		case 0:
+//			cout << "first";
+//		case 1:
+//			cout << "second";
+//			break;
+//		default:
+//			cout << "hello";
+//		}
+//	case 2:
+//		cout << "third";
+//	}
 //	return 0;
 //}
