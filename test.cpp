1343c80007d59e87482416c05d318a0f9fbfdf03
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-15 23:56:00 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,14 +1,92 @@
-#include<stdio.h>
+//#include<iostream>
+//#include <stack>
+//using namespace std;
+//int main()
+//{
+//	stack<char>S;
+//	char x, y;
+//	x = 'n';
+//	y = 'g';
+//	S.push(x);
+//	S.push('i');
+//	S.push(y);
+//	S.pop();
+//	S.push('r');
+//	S.push('t');
+//	S.push(x);
+//	S.pop();
+//	S.push('s');
+//	while (!S.empty())
+//	{
+//		x = S.top();
+//		S.pop();
+//		cout << x;
+//	}
+//	cout << y;
+//	return 0;
+//}
+//#include<iostream>
+//#include <stack>
+//#include <queue>
+//using namespace std;
+//int main()
+//{
+//	queue<char>S;
+//	char x, y;
+//	x = 'n';
+//	y = 'g';
+//	S.push(x);
+//	S.push('i');
+//	S.push(y);
+//	S.pop();
+//	S.push('r');
+//	S.push('t');
+//	S.push(x);
+//	S.pop();
+//	S.push('s');
+//	while (!S.empty())
+//	{
+//		x = S.front();
+//		S.pop();
+//		cout << x;
+//	}
+//	cout << y;
+//	return 0;
+//}
+#include<iostream>
+#include <stack>
+#include <queue>
+using namespace std;
 int main()
 {
-	printf("%d\n", sizeof(char));
-	printf("%d\n", sizeof(int));
-	printf("%d\n", sizeof(short));
-	printf("%d\n", sizeof(double));
-	printf("%d\n", sizeof(float));
-	printf("%d\n", sizeof(long));
-	printf("%d\n", sizeof(long long));
-	printf("%d\n", sizeof(long double));
-	printf("%d\n", sizeof(bool));
+	priority_queue<int>a;
+	priority_queue<int, vector<int>, greater<int>>c;
+	priority_queue<string>b;
+	for (int i = 0; i < 5; ++i)
+	{
+		a.push(i);
+		c.push(i);
+	}
+	while (!a.empty())
+	{
+		cout << a.top() << " ";
+		a.pop();
+	}
+	cout << endl;
+	while (!c.empty())
+	{
+		cout << c.top() << " ";
+		c.pop();
+	}
+	cout << endl;
+	b.push("abc");
+	b.push("abcd");
+	b.push("cbd");
+	while (!b.empty())
+	{
+		cout << b.top() << " ";
+		b.pop();
+	}
+	cout << endl;
 	return 0;
 }
