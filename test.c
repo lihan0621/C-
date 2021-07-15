2bfa97c52e981f88fd4aaff97f4bc8402f827df8
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-15 08:21:24 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,63 +1,27 @@
-//#define  _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int Findkey(int arr[], int n, int key)
-//{
-//	for (int i = 0; i < n; i++) {
-//		if (arr[i] == key) {
-//			return i;
-//		}
-//	}
-//	return -1;
-//}
-//int FindMax(int arr[], int n) {
-//	//int max = 0;int arr[] = { -1, -2, -3, -4 };
-//	int max = arr[0];
-//	for (int i = 1; i < n; i++) {
-//		if (arr[i] > max) {
-//			max = arr[i];
-//		}
-//	}
-//	return max;
-//}
-//int FindSecond(int arr[], int n)
-//{
-//	int first;
-//	int second;
-//	if (arr[0] > arr[1]) {
-//		first = arr[0];
-//		second = arr[1];
-//	}
-//	if (arr[0] < arr[1]) {
-//		first = arr[1];
-//		second = arr[0];
-//	}
-//	for (int i = 2; i < n; i++) {
-//		if (arr[i] > first) {
-//			second = first;
-//			first = arr[i];
-//		}
-//		else if (arr[i] > second) {
-//			second = arr[i];
-//		}
-//	}
-//	return second;
-//}
-//int main()
-//{
-//	int arr[] = { 1,84,5,43,27,74,7,8,73,12,4,76 };
-//	int n = sizeof(arr) / sizeof(arr[0]);
-//	//int key = 0;
-//	//scanf("%d", &key);
-//	//int index = Findkey(arr, n, key);
-//	//if (index == -1) {
-//	//	printf("没找到\n");
-//	//}
-//	//else {
-//	//	printf("%d\n", index);
-//	//}
-//	int max = FindMax(arr, n);
-//	printf("%d\n", max);
-//	int second = FindSecond(arr, n);
-//	printf("%d\n", second);
-//	return 0;
-//}
+#include <stdio.h>
+
+//静态顺序表: 空间不变
+typedef int SLDataType;
+#define N 10
+typedef struct seqList1 {
+	SLDataType _data[N];
+	size_t _size;      
+	size_t _capaticy; 
+}seqList1;
+//动态顺序表定义: 空间可变
+typedef struct seqList {
+	SLDataType* _data;//需要动态开辟的数组
+	size_t _size;      //有效元素的个数
+	size_t _capaticy;  //当前可以存放的最大元素个数
+}seqList;
+
+void test1() {
+	printf("静态: %d\n", sizeof(seqList1));
+	printf("动态: %d\n", sizeof(seqList));
+}
+
+int main1() {
+	test();
+	system("pause");
+	return 0;
+}
