9996b6953d971aa2d72d1a674ea7f2d074e4ac2f
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-16 15:42:50 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,51 +1,89 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+template<class T>
+class PriorityQueue
+{
+public:
+	void shiftUp(int child)
+	{
+		int parent = (child - 1) / 2;
+		//调整
+		while (child > 0)
+		{
+			if (v[parent] < v[child])
+			{
+				swap(v[parent], v[child]);
+				child = parent;
+				parent = (child - 1) / 2;
+			}
+			else
+				break;
+		}
+	}
+	void shiftDown()
+	{
+		int parent = 0;
+		int child = 2 * parent + 1;
+		while (child < v.size())
+		{
+			//从左右孩子中找到最大值
+			if (child + 1 < v.size() && v[child] < v[child + 1])
+				++child;
+			if (v[parent] < v[child])
+			{
+				swap(v[parent], v[child]);
+				parent = child;
+				child = 2 * parent + 1;
+			}
+			else
+				break;
+		}
+	}
+	void push(const T& val)
+	{
+		v.push_back(val);
+		shiftUp(v.size() - 1);
+	}
+	void pop()
+	{
+		swap(v[0], v[v.size() - 1]);
+		v.pop_back();
+		shiftDown();
+	}
+	T& top()
+	{
+		return v[0];
+		//return v.front();
+	}
+	size_t size() const
+	{
+		return v.size();
+	}
+	bool empty() const
+	{
+		return v.empty();
+	}
+private:
+	vector<T> v;
+};
+void test()
+{
+	PriorityQueue<int> q;
+	q.push(100);
+	q.push(1);
+	q.push(150);
+	q.push(10);
+	q.push(20);
+	while (!q.empty())
+	{
+		cout << q.top() << " ";
+		q.pop();
+	}
+	cout << endl;
+}
 int main()
 {
-	/*vector<int>array;
-	array.push_back(100);
-	array.push_back(300);
-	array.push_back(300);
-	array.push_back(300);
-	array.push_back(300);
-	array.push_back(500);
-	vector<int>::iterator itor;
-	for (itor = array.begin(); itor != array.end(); ++itor)
-	{
-		if (*itor == 300)
-		{
-			itor = array.erase(itor);
-		}
-	}
-	for (itor = array.begin(); itor != array.end(); ++itor)
-	{
-		cout << *itor << " ";
-	}*/
-	/*int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
-	int n = sizeof(arr) / sizeof(int);
-	vector<int>v(arr, arr + n);
-	cout << v.size() << ":" << v.capacity() << endl;
-	v.reserve(100);
-	v.resize(20);
-	cout << v.size() << ":" << v.capacity() << endl;
-	v.reserve(50);
-	v.resize(5);
-	cout << v.size() << ":" << v.capacity() << endl;*/
-	int arr[] = { 1,2,3,4,5,6,7,8,9 };
-	int n = sizeof(arr) / sizeof(int);
-	vector<int>v(arr, arr + n);
-	vector<int>::iterator it = v.begin();
-	while (it != v.end())
-	{
-		if (*it != 0)
-		{
-			cout << *it;
-		}
-		else {
-			v.erase(it);
-		}
-		++it;
-	}
+	test();
 	return 0;
 }
