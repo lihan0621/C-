cfd459af9b79eeeb308fc8fe2d33b0d6b89ec65a
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-19 00:18:15 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,169 +1,202 @@
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<string.h>
-#include<conio.h>
-#include<thread>
+#include<iostream>
 using namespace std;
-#define ROWSIZE 23
-#define COLSIZE 62
-#define LETSIZE 26
-#define MAXLET  10
-typedef char Array[ROWSIZE][COLSIZE];
-typedef struct
+class Date
 {
-	int tag;
-	int row;
-	int col;
-}LetNode;
-typedef struct
-{
-	LetNode data[LETSIZE];
-	int curnum;
-	int maxnum;
-}LetTable;
-void InitArray(Array ar)
-{
-	memset(ar, ' ', sizeof(Array));
-	memset(ar[0], '_', sizeof(char)*COLSIZE);
-	memset(ar[ROWSIZE - 1], '_', sizeof(char)*COLSIZE);
-	for (int i = 0; i < ROWSIZE; ++i)
+public:
+	Date(int y = 1, int m = 1, int d = 1)
 	{
-		ar[i][COLSIZE - 2] = '\n';
-		ar[i][COLSIZE - 1] = '\0';
-	}
-}
-void InitLetTable(LetTable *plet, int m)
-{
-	srand(unsigned int(time(NULL)));
-	int i = 0;
-	//int let[26] = { 0 };
-	int col[COLSIZE] = { 0 };
-	memset(plet->data, 0, sizeof(LetNode)*LETSIZE);
-	plet->curnum = m;
-	plet->maxnum = MAXLET;
-	while (i < m)
-	{
-		int index = rand() % 26;
-		int c = rand() % (COLSIZE - 2);
-		if (plet->data[index].tag == 0 && col[c] == 0)
+		//判断日期是否有效
+		if (y <= 0 || m <= 0 || m > 12 || d <= 0 || d > getDay(y, m))
 		{
-			plet->data[index].tag = 1;
-			col[c] = 1;
-			plet->data[index].col = c;
-			plet->data[index].row = 1;
-			++i;
+			_y = 1;
+			_m = 1;
+			_d = 1;
+			cout << "日期无效,默认为1-1-1" << endl;
 		}
-	}
-}
-void PrintArray(Array ar, LetTable *plet)
-{
-	system("cls");
-	InitArray(ar);
-	for (int i = 0; i < LETSIZE; ++i)
-	{
-		if (plet->data[i].tag == 1)
-		{
-			ar[plet->data[i].row][plet->data[i].col] = i + 'a';
-		}
-	}
-	for (int i = 0; i < ROWSIZE; ++i)
-	{
-		printf("%s", ar[i]);
-	}
-}
-void LetDown(LetTable *plet)
-{
-	for (int i = 0; i < LETSIZE; ++i)
-	{
-		if (plet->data[i].tag == 1)
-		{
-			plet->data[i].row += 1;
-		}
-	}
-}
-bool Romvex(char ch, LetTable *plet)
-{
-	bool res = false;
-	/*for (int i = 0; i < LETSIZE; ++i)
-	{
-	if (x[i].ch == ch)
-	{
-	x[i].ch = '\0';
-	x[i].ch = rand() % 26 + 'a';
-	x[i].row = 1;
-	x[i].col = rand() % (COLSIZE - 2);
-	}
-	}*/
-	//x[ch - 'a'].ch = 0;
-	int index = ch - 'a';
-	if (plet->data[index].tag == 0) return true;
-	plet->data[index].tag = 0;
-	while (1)
-	{
-		index = rand() % 26;
-		if (plet->data[index].tag == 0)
-		{
-			plet->data[index].tag = 1;
-			plet->data[index].row = 1;
-			plet->data[index].col = rand() % (COLSIZE - 2);
-			break;
+		else
+		{		
+			_y = m;
+			_m = m;
+			_d = d;
 		}
 
 	}
-	return res;
-}
-bool is_Down(LetTable *plet)
-{
-	bool flag = false;
-	for (int i = 0; i < LETSIZE; ++i)
+	int getDay(int y, int m)
 	{
-		if (plet->data[i].tag == 1 && plet->data[i].row >= ROWSIZE)
+		static int days[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int day = days[m];
+		//如果是二月,且为闰年,修改天数
+		if ((m == 2) && ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)))
 		{
-			flag = true;
-			break;
+			day += 1;
+			return day;
 		}
 	}
-	return flag;
-}
-int main()
-{
-	int num = 0;
-	Array ar;
-	LetTable x;
-	char ch;
-	InitLetTable(&x, 1);
-	int wait = 1000;
-	while (1)
+	//日期运算
+	//Date + int    int a,int b,a += b;
+	//左操作数 += 右操作数 :左操作数的内容发生变化,返回值:相加之后的值
+	Date& operator+=(int day)
 	{
-		PrintArray(ar, &x);
-		LetDown(&x);
-		if (is_Down(&x))
+		//判断日期是否为负
+		if (day<0)
 		{
-			break;
+			return *this -= -day;
 		}
-		if (_kbhit())
+		//相加天数
+		_d += day;
+		//判断天数是否溢出
+		while (_d > getDay(_y, _m))
 		{
-			ch = _getch();
-			if (Romvex(ch, &x))
+			//减去当月的天数
+			_d -= getDay(_y, _m);
+			//月份进位
+			++_m;
+			//判断月份是否溢出
+			if (_m == 13)
 			{
-				num += 1;
-				if (num % 5 == 0)
+				//年份进位
+				++_y;
+				_m = 1;
+			}
+		}
+		//返回相加之后的结果
+		return *this;
+	}
+	//返回相加之后的结果
+	//Date + int
+	Date operator+(int day)
+	{
+		Date copy(*this);
+		//Data copy = *this
+		copy += day;
+		return copy;
+	}
+	//前置++
+	//++Date
+	Date& operator++()
+	{
+		return *this += 1;
+	}
+	//后置++
+	//Date++
+	//int参数的作用:只是为了和前置++构成重载,函数内部不会用到
+	Date operator++(int)
+	{
+		Date copy(*this);
+		*this += 1;
+		return copy;
+	}
+	Date& operator-=(int day)
+	{
+		_d -= day;
+		while (_d <= 0)
+		{
+			//用上一月的天数回补
+			--_m;
+			//判断月份是否溢出(向下)
+			if (_m == 0)
+			{
+				//需要退回上一年的12月份
+				--_y;
+				_m = 12;
+			}
+			_d += getDay(_y, _m);
+		}
+		return *this;
+	}
+	//Date - int
+	Date operator-(int day)
+	{
+		Data copy(*this);
+		copy -= day;
+		return copy;
+	}
+	//--Date
+	Data operator--()
+	{
+		return *this -= 1;
+	}
+	//Date--
+	Date& operator--(int)
+	{
+		Date copy(*this);
+		*this -= 1;
+		return copy;
+	}
+	//==
+	bool operator==(const Date& d)
+	{
+		return _y == d._y
+			&& _m == d._m
+			&& _d == d._d;
+	}
+	//!=
+	bool operator!=(const Date& d)
+	{
+		return !(*this == d);
+	}
+	//>
+	bool operator>(const Date& d)
+	{
+		if (_y > d._y)
+		{
+			return true;
+		}
+		else if (_y == d.y)
+		{
+			if (_m > d._m)
+			{
+				return true;
+			}
+			else if (_m == d._m)
+			{
+				if (_d > d._d)
 				{
-					if (x.curnum < x.maxnum)
-					{
-						wait = wait / 2;
-					}
-					//else
-					//{
-
-					//}
+					return true;
 				}
 			}
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(wait));
+		return false;
 	}
-	system("pause");
-	return 0;
-}
+	//<
+	bool operator>(const Date& d)
+	{
+		return !(*this >= d);
+	}
+	//>=
+	bool operator>=(const Date& d)
+	{
+		return *this > d || *this == d;
+	}
+	//<=
+	bool operator<=(const Date& d)
+	{
+		return !(*this > d);
+	}
+	//日期相减:Date - Data
+	int operator-(const Date& d)
+	{
+		//计算比较小的日期经过多少次的自加运算可以和较大日期相等
+		//自加的天数就是两个日期相差的天数
+		Date max = *this;
+		Date min = d;
+		int flag = 1;
+		if (d > *this)
+		{
+			max = d;
+			min = *this;
+			flag = -1;
+		}
+		int day = 0;
+		while (min < max)
+		{
+			++min;
+			++day;
+		}
+		return flag * day;
+	}
+private:
+	int _y;
+	int _m;
+	int _d;
+};
