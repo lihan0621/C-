ac16623529a713a383172c0825c1de306ca5f737
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-20 11:00:22 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,27 +1,146 @@
 #include <stdio.h>
 
-//静态顺序表: 空间不变
-typedef int SLDataType;
-#define N 10
-typedef struct seqList1 {
-	SLDataType _data[N];
-	size_t _size;      
-	size_t _capaticy; 
-}seqList1;
-//动态顺序表定义: 空间可变
-typedef struct seqList {
-	SLDataType* _data;//需要动态开辟的数组
-	size_t _size;      //有效元素的个数
-	size_t _capaticy;  //当前可以存放的最大元素个数
-}seqList;
+typedef int LDataType;
 
-void test1() {
-	printf("静态: %d\n", sizeof(seqList1));
-	printf("动态: %d\n", sizeof(seqList));
+//双向带头循环列表的节点
+typedef struct ListNode {
+	LDataType _data;
+	//指向下一个节点的起始位置
+	struct ListNode* _next;
+	//指向上一个节点的起始位置
+	struct ListNode* _prev;
+}ListNode;
+
+//双向带头循环链表
+typedef struct List {
+	struct ListNode* _head;
+}List;
+
+//创建节点
+struct ListNode* createListNode(LDataType val) {
+	struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+	node->_data = val;
+	node->_next = NULL;
+	node->_prev = NULL;
+	return node;
 }
 
-int main1() {
-	test();
-	system("pause");
-	return 0;
+//初始化链表
+void ListInit(List* lst) {
+	if (lst == NULL)
+		return;
+	//空链表
+	lst->_head = createListNode(0);
+	lst->_head->_next = lst->_head->_prev = lst->_head;
+}
+
+//尾插
+void listPushBack(List* lst, LDataType val) {
+	/*	
+	if (lst == NULL)
+		return;
+	struct ListNode* last = lst->_head->_prev;
+	struct ListNode* newNode = createListNode(val);
+	//head ...... last newNode
+	last->_next = newNode;
+	newNode->_prev = last;
+	newNode->_next = lst->_head;
+	lst->_head->_prev = newNode;
+	*/
+
+	listInsert(&lst, lst->_head, val);
+}
+
+//尾删
+void listPopBack(List* lst) {
+	/*
+	if (lst == NULL)
+		return;
+	if (lst->_head->_next == lst->_head)
+		return;
+	struct ListNode* last = lst->_head->_prev;
+	struct ListNode* prev = last->_prev;
+	free(last);
+	lst->_head->_prev = prev;
+	prev->_next = lst->_head;
+	*/
+	listErase(lst, lst->_head->_prev);
+}
+
+//输出
+void printlist(List* lst) {
+	struct ListNode* cur = lst->_head->_next;
+	while (cur != lst->_head) {
+		printf("%d ", cur->_data);
+		cur = cur->_next;
+	}
+	printf("\n");
+}
+
+//头插
+void listPushFront(List* lst, LDataType val) {
+	/*
+	if (lst == NULL)
+		return;
+	struct ListNode* next = lst->_head->_next;
+	struct ListNode* newNode = createListNode(val);
+	lst->_head->_next = newNode;
+	newNode->_prev = lst->_head;
+	newNode->_next = next;
+	next->_prev = newNode;
+	*/
+	listInsert(lst, lst->_head->_next, val);
+}
+
+//头删
+void listPopFront(List* lst) {
+	/*
+	if (lst == NULL || lst->_head->_next == lst->_head)
+		return;
+	struct ListNode* next = lst->_head->_next;
+	struct ListNode* nextnext = next->_next;
+	lst->_head->_next = nextnext;
+	nextnext->_prev = lst->_head;
+	free(next);
+	*/
+	listErase(lst, lst->_head->_next);
+}
+
+//删除任意位置的节点
+void listErase(List* lst, struct ListNode* node) {
+	if (lst == NULL || node == lst->_head)
+		return;
+	struct ListNode* prev = node->_prev;
+	struct ListNode* next = node->_next;
+	prev->_next = next;
+	next->_prev = prev;
+	free(node);
+}
+
+//在当前节点之前插入新节点
+void listInsert(List* lst, struct ListNode* node, LDataType val) {
+	if (lst == NULL)
+		return;
+	struct ListNode* prev = node->_prev;
+	struct ListNode* newNode = createListNode(val);
+	// prev  newNode  node
+	prev->_next = newNode;
+	newNode->_prev = prev;
+	newNode->_next = node;
+	node->_prev = newNode;
+}
+
+//销毁
+listDestroy(List* lst) {
+	if (lst) {
+		if (lst->_head) {
+			struct ListNode* cur = lst->_head->_next;
+			while (cur != lst->_head) {
+				struct ListNode* next = cur->_next;
+				free(cur);
+				cur = next;
+			}
+			free(lst->_head);
+		}
+	}
 }
