8e9d7cad8251837999d5a0d578b103585472ca34
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-07-29 15:26:12 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,257 +1,153 @@
-#include <iostream>
 #include <stdlib.h>
+#include <iostream>
 
 using namespace std;
 
-class Date; // 前置声明
-class Time
-{
-	// 声明日期类为时间类的友元类，则在日期类中就直接访问Time类中的私有成员变量	
-	friend class Date;
-public:
-	Time(int hour = 0, int minute = 0, int second = 0)
-		: _hour(hour)
-		, _minute(minute)
-		, _second(second)
-	{}
-
-	void fun(Date& d)
-	{
-		cout << d._year << d._month << d._day << endl;
-	}
-
-private:
-	int _hour;
-	int _minute;
-	int _second;
-};
-class Date
-{
-	friend class Time;
-public:
-	Date(int year = 1900, int month = 1, int day = 1)
-		: _year(year)
-		, _month(month)
-		, _day(day)
-	{}
-	//友元类中的所有函数变成另一个类的友元函数
-	void SetTimeOfDate(int hour, int minute, int second)
-	{
-		// 直接访问时间类私有的成员变量
-		_t._hour = hour;
-		_t._minute = minute;
-		_t._second = second;
-	}
-private:
-	int _year;
-	int _month;
-	int _day;
-	Time _t;
-};
 
 void test()
 {
-
+	//new: operator new--->malloc
+	int* ptr = new int;
+	//delete: operator delete-->free
+	delete ptr;
 }
 
-
-//class Date
-//{
-//public:
-//	//友元函数的声明：此函数可以访问当前类的私有成员
-//	//此函数是一个普通函数，不属于类的成员函数
-//	//友元的声明可以放在类的任何地方，不影响使用
-//	friend ostream& operator<<(ostream& _cout, const Date& d);
-//	Date(int y = 1, int m = 2, int d = 3)
-//		:_y(y)
-//		,_m(m)
-//		,_d(d)
-//	{}
-//	//void operator<<(ostream& _cout)
-//	//{
-//	//	_cout << _y << "-" << _m << "-" << _d << endl;
-//	//}
-//private:
-//	int _y;
-//	int _m;
-//	int _d;
-//};
-//
-////返回值为ostream，支持连续输出
-//ostream& operator<<(ostream& _cout, const Date& d)
-//{
-//	_cout << d._y << "-" << d._m << "-" << d._d << endl;
-//	return _cout;
-//}
-//
+//void* operator new(size_t n);
 //void test()
 //{
-//	Date d(2021, 7, 28);
-//	//d.operator<<(cout);
-//	//d << cout;
-//	cout << d;
-//	//错误
-//	//d.operator<<(cout, d);
-//	//连续输出
-//	cout << d << endl;
+//	//通过这两种方式就可以看出：operator new不是new的运算符重载函数
+//	//void* ptr = operator new(sizeof(int));
+//	//void* ptr2 = new sizeof(int);
+//	//同理：operator delete也不是delete的运算符重载函数
+//	//operator new和operator delete是C++中的全局函数
+//	//它们的使用方式类似于malloc和free
+//	//operator new：malloc + 异常
+//	int* ptr = (int*)operator new(sizeof(int));
+//	int* ptr2 = (int*)malloc(sizeof(int));
+//
+//	//operator delete：封装了free
+//	operator delete(ptr);
+//	free(ptr2);
 //}
 
 //class A
 //{
 //public:
-//	A()
+//	/*A()
 //	{
-//		++_count;
-//	}
-//	A(const A& a)
-//	{
-//		++_count;
-//	}
-//	int getCount()
-//	{
-//		return _count;
-//	}
-////private:
-//	//static成员是所有对象共享
-//	//static成员变量，必须在类外初始化
-//	static int _count;
-//};
-//
-//int A::_count = 0;
-//A funA(A a)
-//{
-//	return a;
-//}
-//
-//void test()
-//{
-//	A a1;
-//	A a2;
-//	A a3 = funA(a1);
-//	cout << a1.getCount() << endl;
-//	cout << a2.getCount() << endl;
-//	cout << a3.getCount() << endl;
-//	//static成员访问形式
-//	//1.对象.static成员
-//	cout << a1._count << endl;
-//	//2.类型 + :: + static成员
-//	cout << A::_count << endl;
-//}
-
-//class A
-//{
-//public:
-//	A(int a)
+//		cout << "A()" << endl;
+//	}*/
+//	A(int a = 10)
 //		:_a(a)
-//	{
-//		cout << "A(int)" << endl;
-//	}
-//
-//	A(const A& aobj)
-//	{
-//		_a = aobj._a;
-//	}
-//private:
-//	int _a;
-//};
-//
-//void test()
-//{
-//	A aobj(10); //构造
-//	A aobj2 = aobj; //拷贝构造
-//	A aobj3 = 20;
-//	aobj3 = 40;
-//}
-
-//class A
-//{
-//public:
-//	//初始化列表中初始化顺序：按照声明顺序，并不是初始化列表中的书写顺序
-//	A(int a)
-//		:_a2(a)
-//		, _a1(_a2)
 //	{}
-//	void Print() {
-//		cout << _a1 << " " << _a2 << endl;
-//	}
-//private:
-//	int _a2;
-//	int _a1;
-//};
-//
-//void test()
-//{
-//	A aa(1);
-//	aa.Print();
-//}
-
-//class Time
-//{
-//public:
-//	Time(int hour, int minute, int second)
-//		:_hour(hour)
-//		,_minute(minute)
-//		,_second(second)
+//	A(int a, int b, int c)
+//		:_a(a)
 //	{}
-//private:
-//	int _hour;
-//	int _minute;
-//	int _second;
-//};
-//
-//class Date
-//{
-//public:
-//	Date(int year, int month, int day)
-//		//初始化列表: 真正初始化的位置
-//		//引用成员必须在初始化列表中进行初始化
-//		:_year(year)
-//		, _month(month)
-//		, _day(day)
-//		, _hour(12)
-//		, _time(12, 50, 34)
+//	~A()
 //	{
-//		//并不是初始化
-//	/*	_year = year;
-//		_month = month;
-//		_day = day;*/
+//		cout << "清理资源" << endl;
+//		cout << "~A()" << endl;
 //	}
 //private:
-//	int& _year;
-//	int& _month;
-//	int& _day;
-//	const int _hour;
-//	Time _time;
+//	int _a = 10;
 //};
 //
 //void test()
 //{
-//	Date d(2021, 7, 28);
+//	A* mpa= (A*)malloc(sizeof(A));
+//	free(mpa);
+//	cout << "---------" << endl;
+//	//自定义类型：new：申请空间 + 调用构造函数进行空间内容的初始化
+//	//类型指针 指针变量 new 类型--->调用默认构造(无参，全缺省)
+//	A* pa1 = new A;
+//	//类型指针 指针变量 new 类型(参数列表)--->调用带参构造
+//	A* pa2 = new A(10);
+//	A* pa3 = new A(1, 2, 3);
+//	//自定义类型：delete：调用析构完成资源清理 + 释放空间
+//	delete pa1;
+//	delete pa2;
+//	delete pa3;
+//
+//	//自定义类型：连续空间
+//	//new[]：申请空间 + 调用N次构造函数，需要有默认构造存在
+//	A* arrA = new A[100];
+//	//不能使用带参的构造进行对个对象的空间申请和初始化
+//	//A* arrA2 = new A(10)[3];
+//	//delete[]：调用N次析构 + 空间释放
+//	delete[] arrA;
 //}
 
-//class Date
+//void test()
 //{
-//public:
-//	Date(int year, int month, int day)
-//		//初始化列表: 真正初始化的位置
-//		:_year(year)
-//		, _month(month)
-//		,_day(day)
-//	{
-//		//并不是初始化
-//	/*	_year = year;
-//		_month = month;
-//		_day = day;*/
-//	}
-//private:
-//	int _year;
-//	int _month;
-//	int _day;
-//};
+//	//申请和释放的方式保持一致
+//	//malloc ---> free
+//	int* mptr = (int*)malloc(sizeof(int));
+//	free(mptr);
+//
+//	//类型指针 指针变量 = new 类型
+//	//类型指针 指针变量 = new 类型(初始值)
+//	//类型指针 指针变量 = new 类型[元素个数]
+//	//new --->delete
+//	int* ptr = new int;
+//	delete ptr;
+//	//申请空间 + 初始化：初始值为10--->4byte
+//	int* ptr2 = new int(10);
+//	delete ptr2;
+//	//new[] ---> delete[]
+//	//连续空间：包含10个元素--->40byte，内容为随机值
+//	int* arr = new int[10];
+//	//释放连续空间
+//	delete[] arr;
+//}
+
+//void test()
+//{
+//	//malloc只负责申请空间，不负责初始化
+//	char* ptr = (char*)malloc(sizeof(char));
+//	//调整空间大小
+//	char* ptr2 = (char*)realloc(ptr, 2 * sizeof(char));
+//	//申请新的空间，功能和malloc相同
+//	char* ptr3 = (char*)realloc(NULL, sizeof(char));
+//	char* ptr4 = (char*)realloc(ptr3, sizeof(char) * 100);
+//
+//	char* ptr5 = (char*)realloc(NULL, sizeof(char));
+//	char* ptr6 = (char*)realloc(ptr5, sizeof(char) * 100000);
+//
+//	//ptr指向的空间不能显示释放
+//	//只能显式释放realloc返回之后的空间，传入realloc中的指针空间不能显式释放
+//	//free(ptr);
+//	free(ptr2);
+//	//free(ptr3);
+//	free(ptr4);
+//	//free(ptr5);
+//	free(ptr6);
+//
+//	//申请空间 + 全部初始化为0
+//	char* ptr7 = (char*)calloc(4, sizeof(char));
+//}
+
+//void test()
+//{
+//	char* ptr = (char*)malloc(0x7fffffff);
+//	char* ptr2 = (char*)malloc(0xffffffff);
+//}
+
+
+//int globalVar = 1;
+//static int staticGlobalVar = 1;
 //
 //void test()
 //{
-//	Date d(2021, 7, 28);
+//	static int staticVar = 1;
+//	int localVar = 1;
+//	int num1[10] = { 1, 2, 3, 4 };
+//	char char2[] = "abcd";
+//	const char* pChar3 = "abcd";
+//	int* ptr1 = (int*)malloc(sizeof(int) * 4);
+//	int* ptr2 = (int*)calloc(4, sizeof(int));
+//	int* ptr3 = (int*)realloc(ptr2, sizeof(int) * 4);
+//	free(ptr1);
+//	free(ptr3);
 //}
 
 int main()
