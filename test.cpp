dd1d631df954ef20f93e8c6271ea111b500c9933
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-02 09:26:22 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,130 +1,169 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<list>
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<conio.h>
+#include<thread>
 using namespace std;
-template <class T>
-class Queue
+#define ROWSIZE 23
+#define COLSIZE 62
+#define LETSIZE 26
+#define MAXLET  10
+typedef char Array[ROWSIZE][COLSIZE];
+typedef struct
 {
-	
-public:
-	void push(const T& val)
-	{
-		_q.push_back(val);
-		//_q.push_front(val);
-	}
-	void pop()
-	{
-		_q.pop_front();
-		//_q.pop_back();
-	}
-	T& front()
-	{
-		return _q.front();
-		//return _q.back();
-	}
-	size_t size()
-	{
-		return _q.size();
-	}
-	bool empty()
-	{
-		return _q.empty();
-	}
-	//用list实现
-private:
-	list<T> _q;
-};
-void test()
+	int tag;
+	int row;
+	int col;
+}LetNode;
+typedef struct
 {
-	Queue<int> q;
-	q.push(1);
-	q.push(2);
-	q.push(3);
-	q.push(4);
-	while (!q.empty())
+	LetNode data[LETSIZE];
+	int curnum;
+	int maxnum;
+}LetTable;
+void InitArray(Array ar)
+{
+	memset(ar, ' ', sizeof(Array));
+	memset(ar[0], '_', sizeof(char)*COLSIZE);
+	memset(ar[ROWSIZE - 1], '_', sizeof(char)*COLSIZE);
+	for (int i = 0; i < ROWSIZE; ++i)
 	{
-		cout << q.front() << " ";
-		q.pop();
+		ar[i][COLSIZE - 2] = '\n';
+		ar[i][COLSIZE - 1] = '\0';
 	}
-	cout << endl;
 }
-template <class t>
-class stack
+void InitLetTable(LetTable *plet, int m)
 {
-public:
-	void push(const t& val)
+	srand(unsigned int(time(NULL)));
+	int i = 0;
+	//int let[26] = { 0 };
+	int col[COLSIZE] = { 0 };
+	memset(plet->data, 0, sizeof(LetNode)*LETSIZE);
+	plet->curnum = m;
+	plet->maxnum = MAXLET;
+	while (i < m)
 	{
-		_st.push_back(val);
+		int index = rand() % 26;
+		int c = rand() % (COLSIZE - 2);
+		if (plet->data[index].tag == 0 && col[c] == 0)
+		{
+			plet->data[index].tag = 1;
+			col[c] = 1;
+			plet->data[index].col = c;
+			plet->data[index].row = 1;
+			++i;
+		}
 	}
-	void pop()
-	{
-		 _st.pop_back();
-	}
-	t& top()
-	{
-		return _st.back();
-	}
-	size_t size() const 
-	{
-		return _st.size();
-	}
-	bool empty() const
-	{
-		return _st.empty();
-	}
-	//用vector实现stack
-private:
-	vector<t> _st;
-};
-#include<list>
-template<class t>
-class Stack2
+}
+void PrintArray(Array ar, LetTable *plet)
 {
-public:
-	void push(const t& val)
+	system("cls");
+	InitArray(ar);
+	for (int i = 0; i < LETSIZE; ++i)
 	{
-		_st.push_front(val);
-		//_st.push_back(val);
+		if (plet->data[i].tag==1)
+		{
+			ar[plet->data[i].row][plet->data[i].col] = i + 'a';
+		}
 	}
-	void pop()
+	for (int i = 0; i < ROWSIZE; ++i)
 	{
-		_st.pop_front();
-		//_st.pop_back();
+		printf("%s", ar[i]);
 	}
-	t& top()
+}
+void LetDown(LetTable *plet)
+{
+	for (int i = 0; i < LETSIZE; ++i)
 	{
-		return _st.front();
-		//return _st.back();
+		if (plet->data[i].tag == 1)
+		{
+			plet->data[i].row += 1;
+		}
 	}
-	size_t size() const
+}
+bool Romvex(char ch, LetTable *plet)
+{
+	bool res = false;
+	/*for (int i = 0; i < LETSIZE; ++i)
 	{
-		return _st.size();
-	}
-	bool empty() const
+	if (x[i].ch == ch)
 	{
-		return _st.empty();
+	x[i].ch = '\0';
+	x[i].ch = rand() % 26 + 'a';
+	x[i].row = 1;
+	x[i].col = rand() % (COLSIZE - 2);
 	}
-private:
-	list<t> _st;
-};
-//void test()
-//{
-//	//Stack<int> st;
-//	Stack2 <int> st;
-//	st.push(1);
-//	st.push(2);
-//	st.push(3);
-//	st.push(4);
-//	while (!st.empty())
-//	{
-//		cout << st.top() << " ";
-//		st.pop();
-//	}
-//	cout << endl;
-//}
+	}*/
+	//x[ch - 'a'].ch = 0;
+	int index = ch - 'a';
+	if (plet->data[index].tag == 0) return true;
+	plet->data[index].tag = 0;
+	while (1)
+	{
+		index = rand() % 26;
+		if (plet->data[index].tag == 0)
+		{
+			plet->data[index].tag = 1;
+			plet->data[index].row = 1;
+			plet->data[index].col = rand() % (COLSIZE - 2);
+			break;
+		}
+	
+	}
+	return res;
+}
+bool is_Down(LetTable *plet)
+{
+	bool flag = false;
+	for (int i = 0; i < LETSIZE; ++i)
+	{
+		if (plet->data[i].tag == 1 && plet->data[i].row >= ROWSIZE)
+		{
+			flag = true;
+			break;
+		}
+	}
+	return flag;
+}
 int main()
 {
-	test();
+	int num = 0;
+	Array ar;
+	LetTable x;
+	char ch;
+	InitLetTable(&x, 5);
+	int wait = 1000;
+	while (1)
+	{
+		PrintArray(ar,&x);
+		LetDown(&x);
+		if (is_Down(&x))
+		{
+			break;
+		}
+		if (_kbhit())
+		{
+			ch = _getch();
+			if (Romvex(ch, &x))
+			{
+				num += 1;
+				if (num % 5 == 0)
+				{
+					if (x.curnum >= x.maxnum)
+					{
+						wait = wait / 2;
+					}
+					//else
+					//{
+
+					//}
+				}
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(wait));
+	}
+	system("pause");
 	return 0;
 }
