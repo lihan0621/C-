39127fe85c98f06c895179a3cd49f01fceb071bb
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-06 17:21:11 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,166 +1,387 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <list>
 using namespace std;
 
+//list实现：双向带头的循环链表
+
 template<class T>
-void printList(list<T>& lst)
+struct ListNode
 {
-	typename list<T>::iterator it = lst.begin();
-	while (it != lst.end())
+	T _data;
+	ListNode<T>* _next;
+	ListNode<T>* _prev;
+
+	ListNode(const T& val = T())
+		:_data(val)
+		, _next(nullptr)
+		, _prev(nullptr)
+	{}
+};
+
+//list迭代器：自定义类型，对节点进行封装，
+//通过操作节点完成迭代器规定的功能
+template<class T>
+struct ListIterator
+{
+	ListIterator(ListNode<T>* node)
+		:_node(node)
+	{}
+
+	//*运算符重载函数
+	//*iterstor: 获取数据本身
+	T& operator*()
 	{
-		cout << *it << " ";
-		++it;
+		return _node->_data;
+	}
+
+	//it->_a
+	T* operator->()
+	{
+		return &_node->_data;
+	}
+
+	//++iterator
+	ListIterator<T>& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+
+	//后置++
+	ListIterator<T> operator++(int)
+	{
+		ListIterator<T> tmp(*this);
+		_node = _node->_next;
+		return tmp;
+	}
+
+	bool operator!=(const ListIterator<T>& it) const
+	{
+		//判断成员指针_node是否指向同一个节点
+		return _node != it._node;
+	}
+
+	bool operator==(const ListIterator<T>& it)
+	{
+		return !(*this != it);
+	}
+
+	//封装节点
+	ListNode<T>* _node;
+};
+
+template<class T>
+struct ConstListIterator
+{
+	ConstListIterator(ListNode<T>* node)
+		:_node(node)
+	{}
+
+	//*运算符重载函数
+	//*iterstor: 获取数据本身
+	const T& operator*()
+	{
+		return _node->_data;
+	}
+
+	//it->_a
+	const T* operator->()
+	{
+		return &_node->_data;
+	}
+
+	//++iterator
+	ConstListIterator<T>& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+
+	//后置++
+	ConstListIterator<T> operator++(int)
+	{
+		ListIterator<T> tmp(*this);
+		_node = _node->_next;
+		return tmp;
+	}
+
+	ConstListIterator operator--()
+	{
+		_node = _node->_prev;
+		return *this;
+	}
+
+	bool operator!=(const ConstListIterator<T>& it) const
+	{
+		//判断成员指针_node是否指向同一个节点
+		return _node != it._node;
+	}
+
+	bool operator==(const ConstListIterator<T>& it)
+	{
+		return !(*this != it);
+	}
+
+	//封装节点
+	ListNode<T>* _node;
+};
+
+
+template<class T>
+class List
+{
+public:
+	typedef ListNode<T> Node;
+	typedef Node* pNode;
+	typedef ListIterator<T> iterator;
+	typedef ConstListIterator<T> constIterator;
+
+	iterator begin()
+	{
+		return iterator(_header->_next);
+	}
+
+	constIterator begin() const
+	{
+		return constIterator(_header->_next);
+	}
+
+	iterator end()
+	{
+		return iterator(_header);
+	}
+
+	constIterator end() const
+	{
+		return constIterator(_header);
+	}
+
+	List()
+		:_header(new Node())
+	{
+		//循环结构
+		_header->_next = _header->_prev = _header;
+	}
+
+	List(size_t n, const T& val = T())
+		:_header(new Node())
+	{
+		_header->_next = _header->_prev = _header;
+		for (size_t i = 0; i < n; ++i)
+		{
+			pushBack(val);
+		}
+	}
+
+	template<class inputIterator>
+	List(inputIterator first, inputIterator last)
+		:_header(new Node())
+	{
+		//循环结构
+		_header->_next = _header->_prev = _header;
+		while (first != last)
+		{
+			pushBack(*first);
+			++first;
+		}
+	}
+
+	void pushBack(const T& val)
+	{
+		insert(end(), val);
+	}
+
+	void pushFront(const T& val)
+	{
+		insert(begin(), val);
+	}
+
+	void insert(iterator pos, const T& val)
+	{
+		pNode cur = pos._node;
+		pNode prev = cur->_prev;
+		pNode newNode = new Node(val);
+		prev->_next = newNode;
+		newNode->_prev = prev;
+		newNode->_next = cur;
+		cur->_prev = newNode;
+	}
+
+	iterator erase(iterator pos)
+	{
+		//判断是否为end迭代器
+		if (pos != end())
+		{
+			pNode cur = pos._node;
+			pNode prev = cur->_prev;
+			pNode next = cur->_next;
+			delete cur;
+			prev->_next = next;
+			next->_prev = prev;
+			//pos迭代器失效
+			//更新迭代器：指向下一个元素
+			return iterator(next);
+		}
+		return pos;
+	}
+
+	void popFront()
+	{
+		erase(begin());
+	}
+
+	void popBack()
+	{
+		erase(--end());
+	}
+
+	~List()
+	{
+		if (_header)
+		{
+			//释放每一个节点的空间
+			pNode node = _header->_next;
+			while (node != _header)
+			{
+				pNode next = node->_next;
+				delete node;
+				node = next;
+			}
+			delete _header;
+			_header = nullptr;
+		}
+	}
+
+	//深拷贝
+	List(const List<T>& lst)
+		:_header(new Node())
+	{
+		//循环结构
+		_header->_next = _header->_prev = _header;
+		//元素拷贝
+		//Node* cur = lst._header->_next;
+		//while (cur != lst._header)
+		//{
+		//	pushBack(cur->_data);
+		//}
+		for (auto& e : lst)
+		{
+			pushBack(e);
+		}
+	}
+
+	//List<T>& operator=(const List<T>& lst)
+	//{
+	//	//释放原有的节点
+	//	Node* cur = _header->_next;
+	//	while (cur != _header)
+	//	{
+	//		Node* next = cur->_next;
+	//		delete cur;
+	//		cur = next;
+	//	}
+	//	_header->_next = _header->_prev = _header;
+	//	//拷贝
+	//	for (auto& e : lst)
+	//	{
+	//		pushBack(e);
+	//	}
+	//}
+
+	List<T>& operator=(List<T> lst)
+	{
+		swap(_header, lst._header);
+		return *this;
+	}
+
+
+private:
+	pNode _header;
+};
+
+template <class T>
+void printList(List<T>& lst)
+{
+	typename List<T>::iterator cit = lst.begin();
+	while (cit != lst.end())
+	{
+		cout << *cit << " ";
+		++cit;
 	}
 	cout << endl;
 }
 
-template<class T>
-void printReverseList(list<T>& lst)
+template <class T>
+void printForList(const List<T>& lst)
 {
-	typename list<T>::reverse_iterator it = lst.rbegin();
-	while (it != lst.rend())
-	{
-		cout << *it << " ";
-		++it;
-	}
-	cout << endl;
-}
-
-
-template<class T>
-void printReverseList2(list<T>& lst)
-{
-	cout << "反向迭代器的正向输出" << endl;
-	typename list<T>::reverse_iterator it = lst.rend();
-	--it;
-	while (it != lst.rbegin())
-	{
-		cout << *it << " ";
-		++it;
-	}
-	cout << *it << endl;
-}
-
-template<class T>
-void printForList(list<T>& lst)
-{
-	for (auto& e : lst)
+	for (const auto& e : lst)
 	{
 		cout << e << " ";
 	}
 	cout << endl;
 }
 
+struct A
+{
+	A(int a = 1, int b = 2, int c = 3)
+		:_a(a)
+		,_b(b)
+		,_c(c)
+	{}
+
+	int _a;
+	int _b;
+	int _c;
+};
+
 void test()
 {
-	int arr[] = { 9,5,2,7 };
-	int arr2[] = { 7,5,2,9 };
-	list<int> lst(arr, arr + 4);
-	list<int> lst2(arr2, arr2 + 4);
-	lst.sort();
-	lst2.sort();
-	lst.merge(lst2);
-	printForList(lst);
+	List<A> lst;
+	lst.pushBack(A());
+	lst.pushBack(A(10, 20, 30));
+	lst.pushBack(A(4, 5, 6));
+	lst.pushBack(A(7, 8, 9));
+	List<A>::iterator it = lst.begin();
+	while (it != lst.end())
+	{
+		//cout << *it << " ";
+		cout << it.operator->()->_a << it.operator->()->_b << it.operator->()->_c << " " << endl;
+		//简写的错误形式
+		//cout << it->->_a << it->->_b << it->->_c;
+		//简写的正确形式
+		cout << it->_a << it->_b << it->_c << " " << endl;
+		++it;
+	}
+	printList(lst);
 }
 
 //void test()
 //{
-//	string str = "12344321";
-//	list<char> lst(str.begin(), str.end());
-//	printForList(lst);
-//	//unique: 在数据有序的情况下，进行元素的去重
-//	lst.unique();
-//	printForList(lst);
-//	lst.sort();
-//	printForList(lst);
-//	lst.unique();
-//	printForList(lst);
+//	List<int> lst;
+//	lst.pushBack(1);
+//	lst.pushBack(2);
+//	lst.pushBack(3);
+//	lst.pushBack(4);
+//	List<int>::iterator it = lst.begin();
+//	while (it != lst.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
 //}
 
 //void test()
 //{
-//	string s = "1234";
-//	string s2 = "abc";
-//	list<char> lst(s.begin(), s.end());
-//	list<char> lst2(s2.begin(), s2.end());
-//	//lst.splice(lst.begin(), lst2);
-//	//lst.splice(lst.begin(), lst2, ++lst2.begin());
-//	lst.splice(lst.begin(), lst2, ++lst2.begin(), lst2.end());
-//	printList(lst);
-//	printList(lst2);
+//	List<int> lst;
+//	lst.pushBack(1);
+//	lst.pushBack(2);
+//	lst.pushBack(3);
+//	lst.pushBack(4);
 //
-//	lst.remove('f');
-//	printList(lst);
+//	List<int> lst2((size_t)10, 5);
 //}
 
-//void test()
-//{
-//	list<int> lst;
-//	lst.push_front(1);
-//	lst.push_back(2);
-//
-//	lst.insert(lst.begin(), 0);
-//	list<int>::iterator it = lst.begin();
-//	cout << *it << endl;
-//
-//	lst.insert(lst.end(), 3);
-//	lst.emplace(lst.begin(), -1);
-//	lst.emplace_back(4);
-//	lst.emplace_front(-2);
-//
-//	list<int> copy = lst;
-//	lst.insert(lst.end(), copy.begin(), copy.end());
-//	for (const auto& e : lst)
-//	{
-//		cout << e << " ";
-//	}
-//	cout << endl;
-//
-//	//插入不会导致迭代器失效
-//	cout << *it << endl;
-//
-//	//删除导致迭代器失效
-//	//erase返回值：迭代器--->被删除元素的下一个元素的位置
-//	it = lst.erase(it);
-//	cout << *it << endl;
-//
-//	cout << lst.size() << endl;
-//	//lst.clear();
-//	//cout << lst.size() << endl;
-//	lst.resize(5, 100);
-//	for (const auto& e : lst)
-//	{
-//		cout << e << " ";
-//	}
-//	cout << endl;
-//	lst.resize(7, 200);
-//	for (const auto& e : lst)
-//	{
-//		cout << e << " ";
-//	}
-//	cout << endl;
-//
-//
-//}
-
-//void test()
-//{
-//	list<int> lst;
-//	list<char> lst2(5, 'a');
-//	string str = "1234567";
-//	list<char> lst3(str.begin(), str.end());
-//	list<char> copy(lst3);
-//
-//	vector<char> v(str.begin(), str.end());
-//	list<char> lst4(v.begin(), v.end());
-//
-//	char arr[] = "abcd";
-//	list<char> lst5(arr, arr + 4);
-//}
-
-int main()
+int main2()
 {
 	test();
 	system("pause");
