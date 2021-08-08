1ff855ad64992f68e962e5209fb3496e8471a3df
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-08 14:16:58 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,22 +1,73 @@
+#define F(x, y) ((x) + (y))
 #include <stdio.h>
 
-void Swap(int* arr, int* a, int* b)
+struct Test1
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+	short a;
+	char b;
+	long c;
+	long d;
+};
+
+struct Test2
+{
+	long a;
+	short b;
+	char c;
+	long d;
+};
+
+struct Test3
+{
+	short a;
+	long b;
+	char c;
+	long d;
+};
 
 void test()
 {
-	int arr[] = { 1,2,3 };
-	Swap(arr, &arr[1], &arr[2]);
-	for (int i = 0; i < 3; ++i)
-		printf("%d ", arr[i]);
+	struct Test1 T1;
+	struct Test2 T2;
+	struct Test3 T3;
+	printf("%d  %d  %d", sizeof(T1), sizeof(T2), sizeof(T3));
+}
+
+void test4()
+{
+	int i = 1, sum = 0;
+	while (i < 10) {
+		sum = sum + 1;
+		i++;
+	}
+	printf("%d  %d\n", i, sum);
+}
+
+void test3()
+{
+	int x;
+	x = 5 > 2 + 1 && 2 || 2 * 4 < 4 - !0;
+	printf("%d\n", x);
+}
+
+void test2()
+{
+	int a = 3, b = 4;
+	printf("%d\n", F(a++, b++));
+}
+
+void test1()
+{
+	char a[20];
+	char* ptr = (char*)a;
+	char* ptr2 = (char*)(a + 5);
+	int n = ptr2 - ptr;
+	printf("%d\n", n);
 }
 
 int main()
 {
 	test();
+	system("pause");
 	return 0;
 }
