6dc0d14e52a2a99580fdd377de88adfa7e255b76
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-09 10:32:23 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
-#include <queue>
+#include <list>
 using namespace std;
+
+template<class T>
+class Queue
+{
+public:
+	void push(const T& val)
+	{
+		_q.push_back(val);
+		//_q.push_front(val);
+	}
+
+	void pop()
+	{
+		_q.pop_front();
+		//_q.pop_back();
+	}
+
+	T& front()
+	{
+		return _q.front();
+		//return _q.back();
+	}
+
+	size_t size() const
+	{
+		return _q.size();
+	}
+
+	bool empty()
+	{
+		return _q.empty();
+	}
+
+	//”√list µœ÷
+private:
+	list<T> _q;
+};
 
 void test()
 {
-	queue<int> q;
+	Queue<int> q;
 	q.push(1);
 	q.push(2);
 	q.push(3);
