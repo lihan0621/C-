afb232fa65bad34bde348fa442eee413b2f02d9c
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-09 09:57:25 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,94 +1,23 @@
-#include <vector>
 #include <iostream>
+#include <queue>
 using namespace std;
-
-template<class T>
-class Stack
-{
-public:
-	void push(const T& val)
-	{
-		_st.push_back(val);
-	}
-
-	void pop()
-	{
-		_st.pop_back();
-	}
-
-	T& top()
-	{
-		return _st.back();
-	}
-
-	size_t size() const
-	{
-		return _st.size();
-	}
-
-	bool empty() const 
-	{
-		return _st.empty();
-	}
-
-	//”√ vector µœ÷stack
-private:
-	vector<T> _st;
-};
-
-#include <list>
-template<class T>
-class Stack2
-{
-public:
-
-	void push(const T& val)
-	{
-		_st.push_front(val);
-		//_st.push_back(val);
-	}
-
-	void pop()
-	{
-		_st.pop_front();
-		//_st.pop_back();
-	}
-
-	T& top()
-	{
-		return _st.front();
-		return _st.back();
-	}
-
-	size_t size() const
-	{
-		return _st.size;
-	}
-
-	bool empty() const
-	{ 
-		return _st.empty();
-	}
-
-private:
-	list<T> _st;
-};
 
 void test()
 {
-	Stack<int> st;
-	st.push(1);
-	st.push(2);
-	st.push(3);
-	st.push(4);
+	queue<int> q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
 
-	while (!st.empty())
+	while (!q.empty())
 	{
-		cout << st.top() << " ";
-		st.pop();
+		cout << q.front() << " ";
+		q.pop();
 	}
 	cout << endl;
 }
+
 int main()
 {
 	test();
