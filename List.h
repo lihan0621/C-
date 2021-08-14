0c14e063c5747bb0dddb94fab5d923af700e8252
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-14 15:00:50 +0800

    Add files via upload

--- List.h
+++ List.h
@@ -250,5 +250,144 @@ void ListEraseByVal(List* plist, Elemtype key)
 	free(p);
 }
 
+/////////////////////////////////////////////////////////////////////
+
+/*
+//定义节点类型
+typedef struct ListNode
+{
+	int data;
+	struct ListNode *next;
+}ListNode;
+
+typedef ListNode* List;
+
+//不带头结点
+void InitList(List *plist)
+{
+	*plist = NULL;
+}
+
+void CreateList_Tail(List *plist)
+{
+	ListNode *p = *plist;
+	for(int i=1; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = NULL;
+
+		if(p == NULL)
+			*plist = s;
+		else
+			p->next = s;
+		p = s;
+	}
+}
+void ShowList(List plist)
+{
+	ListNode *p = plist;
+	while(p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+
+/*
+//带头结点
+void InitList(List *plist)
+{
+	//申请头结点
+	*plist = (ListNode*)malloc(sizeof(ListNode));
+	(*plist)->next = NULL;
+}
+
+void CreateList_Tail(List *plist)
+{
+	ListNode *p = *plist;
+	for(int i=1; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = NULL;
+
+		p->next = s;
+		p = s;
+	}
+}
+void ShowList(List plist)
+{
+	ListNode *p = plist->next;
+	while(p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+
+/*
+//不带头结点
+void InitList(List *plist)
+{
+	*plist = NULL;
+}
+
+
+//1->2->3->4->5->....->10
+void CreateList_Tail(List *plist)
+{
+	*plist = (ListNode*)malloc(sizeof(ListNode));
+	assert(*plist != NULL);
+	(*plist)->data = 1;
+	(*plist)->next = NULL;
+
+	ListNode *p = *plist;  //尾插法
+	for(int i=2; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = NULL;
+
+		//链接节点
+		p->next = s;
+		p = s;
+	}
+}
+
+void CreateList_Front(List *plist)
+{
+	*plist = (ListNode*)malloc(sizeof(ListNode));
+	assert(*plist != NULL);
+	(*plist)->data = 1;
+	(*plist)->next = NULL;
+
+	//头插法
+	for(int i=2; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = *plist;
+		*plist = s;
+	}
+}
+
+void ShowList(List plist)
+{
+	ListNode *p = plist;
+	while(p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+*/
 
 #endif /* _LIST_H_*/
