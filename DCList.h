39a3c87bd95888d4aeba2f859500f388a0bbb0f9
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-08-15 21:40:54 +0800

    Add files via upload

---
+++ DCList.h
@@ -0,0 +1,216 @@
+#pragma once
+#ifndef _DCLIST_H_
+#define _DCLIST_H_
+
+#include "Common.h"
+
+typedef struct DCListNode
+{
+	Elemtype data;
+	struct DCListNode* prev;
+	struct DCListNode* next;
+}DCListNode;
+
+typedef struct DCList
+{
+	DCListNode* head;
+}DCList;
+
+//函数声明
+DCListNode* _creatNode(Elemtype v);
+void DCListInit(DCList* plist);
+void DCListPushBack(DCList* plist, Elemtype v);
+void DCListPushFront(DCList* plist, Elemtype v);
+void DCListPopBack(DCList* plist);
+void DCListPopFront(DCList* plist);
+void DCListShow(DCList* plist);
+void DCListInsertByVal(DCList* plist, Elemtype v);
+DCListNode* DCListFind(DCList* plist, Elemtype key);
+void DCListEraseByVal(DCList* plist, Elemtype key);
+void DCListSort(DCList* plist);
+int DCListLength(DCList* plist);
+void DCListClear(DCList* plist);
+
+
+
+//函数实现
+DCListNode* _creatNode(Elemtype v)
+{
+	DCListNode* s = (DCListNode*)malloc(sizeof(DCListNode));
+	assert(s != NULL);
+	s->data = v;
+	s->next = s->prev = s;
+	return s;
+}
+
+void DCListInit(DCList* plist)
+{
+	plist->head = _creatNode(-1);
+}
+
+void DCListPushBack(DCList* plist, Elemtype v)
+{
+	//创建一个新节点
+	DCListNode* s = _creatNode(v);
+
+	//构造新的连接
+	s->next = plist->head;
+	s->prev = plist->head->prev;
+	s->next->prev = s;
+	s->prev->next = s;
+}
+
+void DCListPushFront(DCList* plist, Elemtype v)
+{
+	//创建新节点
+	DCListNode* s = _creatNode(v);
+
+	s->next = plist->head->next;
+	s->prev = plist->head;
+	s->next->prev = s;
+	s->prev->next = s;
+}
+
+void DCListPopBack(DCList* plist)
+{
+	if (plist->head->next == plist->head)
+		return;
+	DCListNode* p = plist->head->prev;
+	p->prev->next = plist->head;
+	plist->head->prev = p->prev;
+	free(p);
+}
+
+void DCListPopFront(DCList* plist)
+{
+	if (plist->head->next == plist->head)
+		return;
+	DCListNode* p = plist->head->next;
+	p->next->prev = plist->head;
+	plist->head->next = p->next;
+	free(p);
+}
+
+void DCListShow(DCList* plist)
+{
+	if (plist->head->next == plist->head)
+		return;
+	DCListNode* p = plist->head->next;
+	while (p != plist->head)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+
+void DCListInsertByVal(DCList* plist, Elemtype v)
+{
+	//创建节点
+	DCListNode* s = _creatNode(v);
+
+	DCListNode* p = plist->head->next;
+	while (p != plist->head && v > p->data)
+		p = p->next;
+
+	s->next = p;
+	s->prev = p->prev;
+	s->next->prev = s;
+	s->prev->next = s;
+}
+
+DCListNode* DCListFind(DCList* plist, Elemtype key)
+{
+	DCListNode* p = plist->head->next;
+	while (p != plist->head && p->data != key)
+		p = p->next;
+
+	if (p == plist->head)
+		return NULL;
+	else
+		return p;
+}
+
+void DCListEraseByVal(DCList* plist, Elemtype key)
+{
+	DCListNode* p = plist->head->next;
+	while (p != plist->head && p->data != key)
+		p = p->next;
+	if (p == plist->head)
+		return;
+	p->next->prev = p->prev;
+	p->prev->next = p->next;
+	free(p);
+}
+
+void DCListSort(DCList* plist)
+{
+	if (plist->head->next == plist->head)
+		return;
+	DCListNode* p = plist->head->next->next;
+	DCListNode* prev = plist->head->prev;
+
+	//截断链表
+	plist->head->next->next = plist->head;
+	plist->head->prev = plist->head->next;
+
+	prev->next = p;
+	p->prev = prev;
+
+	DCListNode* nq = p;
+	while (p->next != p)
+	{
+		DCListNode* q = p->next;
+		if (p->data < plist->head->next->data)
+		{
+			p->next = plist->head->next;
+			p->prev = plist->head;
+			p->next->prev = p;
+			p->prev->next = p;
+		}
+		else
+		{
+			DCListNode* np = plist->head->next;
+			while (np->next != plist->head && p->data > np->next->data)
+				np = np->next;
+			p->prev = np->next->prev;
+			p->next = np->next;
+			p->next->prev = p;
+			p->prev->next = p;
+		}
+		p = q;
+		if(p == nq)
+			break;
+	}
+}
+
+int DCListLength(DCList* plist)
+{
+	DCListNode* p = plist->head->next;
+	int len = 0;
+	while (p != plist->head)
+	{
+		len++;
+		p = p->next;
+	}
+	return len;
+}
+
+void DCListClear(DCList* plist)
+{
+	DCListNode* p = plist->head->next;
+	DCListNode* next = p->next;
+	while (p != plist->head)
+	{
+		p->next = p->prev = NULL;
+		free(p);
+		p = next;
+	}
+	free(plist->head);
+	plist->head->next = plist->head->prev = NULL;
+}
+
+
+
+
+#endif /* _DCLIST_H_ */
