748a8740cf08ab9e7fc1d0280b6dbd56d199e3fb
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-09-18 12:00:40 +0800

    Add files via upload

--- test.9.16.cpp
+++ test.9.16.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory> 
+#include <thread>
 using namespace std;
 
 struct Date
@@ -15,29 +16,186 @@ struct Date
 };
 
 template <class T>
-class UniquePtr
+class SharedPtr
 {
 public:
-	UniquePtr(T* ptr)
+	SharedPtr(T* ptr)
 		:_ptr(ptr)
+		, _countPtr(new size_t(1))
+		, _mtx(new mutex)
 	{}
 
-	//防拷贝
-	UniquePtr(const UniquePtr<T>& up) = delete;
-	UniquePtr<T>& operator=(const UniquePtr<T>& up) = delete;
-
-	~UniquePtr()
+	SharedPtr(const SharedPtr<T>& sp)
+		:_ptr(sp._ptr)
+		, _countPtr(sp._countPtr)
+		, _mtx(sp._mtx)
 	{
-		if (_ptr)
+		//计数器累加
+		//++(*_countPtr);
+		addCount();
+	}
+
+	SharedPtr<T>& operator=(const SharedPtr<T>& sp)
+	{
+		//if (this != &sp)
+		//判断管理的是否为同一份资源
+		if(_ptr != sp._ptr)
+		{
+			//计数器自减
+			//如果计数器为0，当前对象是最后一个管理此资源的对象
+			//负责对资源的释放
+			//if (--(*_countPtr) == 0)
+			if(subCount() == 0)
+			{
+				delete _ptr;
+				delete _countPtr;
+				delete _mtx;
+			}
+			_ptr = sp._ptr;
+			_countPtr = sp._countPtr;
+
+			//计数器累加
+			//++(*_countPtr);
+			addCount();
+		}
+		return *this;
+	}
+
+	~SharedPtr()
+	{
+		//计数器自减
+		//if (--(*_countPtr) == 0)
+		if(subCount() == 0)
 		{
 			delete _ptr;
+			delete _countPtr;
+			delete _mtx;
+			_countPtr = nullptr;
 			_ptr = nullptr;
+			_mtx = nullptr;
 		}
 	}
 
+	T* operator->()
+	{
+		return _ptr;
+	}
+
+	T& operator*()
+	{
+		return *_ptr;
+	}
+
+	size_t getCount()
+	{
+		return *_countPtr;	
+	}
+
+	size_t addCount()
+	{
+		_mtx->lock();
+		++(*_countPtr);
+		_mtx->unlock();
+		return *_countPtr;
+	}
+
+	size_t subCount()
+	{
+		_mtx->lock();
+		--(*_countPtr);
+		_mtx->unlock;
+		return *_countPtr;
+	}
 private:
 	T* _ptr;
+	size_t* _countPtr;
+	//每一份资源有一个独立的锁
+	mutex* _mtx;
 };
+
+void fun(const SharedPtr<Date>& sp, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		//修改引用计数，是线程安全
+		SharedPtr<Date> copy(sp);
+
+		//对于数据的操作不是线程安全的
+		copy->_y++;
+	}
+}
+
+void test()
+{
+	SharedPtr<Date> sp(new Date);
+	SharedPtr<Date> sp2(new Date);
+	int n = 10;
+	//{t1, t2}和{t3, t4}在修改引用计数时，是并行的
+	//t1和t2在修改引用计数时，是串行的
+	//t3和t4在修改引用计数时，是串行的
+	thread t1(fun, ref(sp), n);
+	thread t2(fun, ref(sp), n);
+	thread t3(fun, ref(sp2), n);
+	thread t4(fun, ref(sp2), n);
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+	cout << sp.getCount() << endl;
+	cout << sp2.getCount() << endl;
+}
+
+//void test()
+//{
+//	SharedPtr<Date> sp(new Date);
+//	SharedPtr<Date> sp2(new Date);
+//	SharedPtr<Date> copy(sp);
+//	sp2 = sp;
+//}
+
+//void test()
+//{
+//	shared_ptr<Date> sp(new Date);
+//	cout << sp.use_count() << endl;
+//	shared_ptr<Date> sp2(sp);
+//	cout << sp.use_count() << endl;
+//	cout << sp2.use_count() << endl;
+//	shared_ptr<Date> sp3(new Date);
+//	cout << sp.use_count() << endl;
+//	cout << sp2.use_count() << endl;
+//	cout << sp3.use_count() << endl;
+//
+//	sp3 = sp;
+//	sp3 = sp2;
+//	sp->_y = 2021;
+//	sp2->_m = 9;
+//	sp3->_d = 18;
+//}
+
+//template <class T>
+//class UniquePtr
+//{
+//public:
+//	UniquePtr(T* ptr)
+//		:_ptr(ptr)
+//	{}
+//
+//	//防拷贝
+//	UniquePtr(const UniquePtr<T>& up) = delete;
+//	UniquePtr<T>& operator=(const UniquePtr<T>& up) = delete;
+//
+//	~UniquePtr()
+//	{
+//		if (_ptr)
+//		{
+//			delete _ptr;
+//			_ptr = nullptr;
+//		}
+//	}
+//
+//private:
+//	T* _ptr;
+//};
 
 //void test()
 //{
