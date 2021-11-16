d21e3cfc8b0a6d52caa44d1e638532afcd5b22f9
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-11-16 21:13:51 +0800

    Add files via upload

--- Sort.h
+++ Sort.h
@@ -55,18 +55,132 @@ void InsertSort_3(int* ar, int left, int right)
 	}
 }
 
+//直接插入排序--哨兵位
+void InsertSort_4(int* ar, int left, int right)
+{
+	for (int i = left + 1; i < right; ++i)
+	{
+		ar[0] = ar[i];   //哨兵位
+		int j = i;
+		while (ar[0] < ar[j - 1])
+		{
+			ar[j] = ar[j - 1];
+			j--;
+		}
+		ar[j] = ar[0];
+	}
+}
+
+//折半插入排序
+void BinInsertSort(int* ar, int left, int right)
+{
+	for (int i = left + 1; i < right; ++i)
+	{
+		int tmp = ar[i];
+		int low = 0;
+		int high = i - 1;
+		int mid;
+		while (low <= high)   //折半查找插入位置
+		{
+			mid = (high + low) / 2;
+			if (tmp >= ar[mid])
+				low = mid + 1;
+			if (tmp < ar[mid])
+				high = mid - 1;
+		}
+		for (int j = i; j > low; --j)
+			ar[j] = ar[j - 1];
+		ar[low] = tmp;
+	}
+}
+
+//二路插入排序  空间复杂度O(n)
+void TwoWayInsertSort(int* ar, int left, int right)
+{
+	int n = right - left;
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	assert(tmp != NULL);
+	tmp[0] = ar[left];
+	int first, final;
+	first = final = 0;
+	for (int i = left + 1; i < right; ++i)
+	{
+		if (ar[i] < tmp[first])
+		{
+			first = (first - 1 + n) % n;
+			tmp[first] = ar[i];
+		}
+		else if (ar[i] >= tmp[final])
+		{
+			tmp[++final] = ar[i];
+		}
+		else
+		{
+			int end = final;
+			while (ar[i] < tmp[end])
+			{
+				tmp[(end + 1) % n] = tmp[end];
+				end = (end - 1 + n) % n;
+			}
+			tmp[(end + 1) % n] = ar[i];
+			final++;
+		}
+	}
+	int k = 0;
+	for (int i = first; k < n; ++k)
+	{
+		ar[k] = tmp[i];
+		i = (i + 1) % n;
+	}
+	free(tmp);
+}
+
+//希尔排序
+void _ShellInsert(int* ar, int left, int right, int gap)
+{
+	for (int i = left + gap; i < right; ++i)
+	{
+		int tmp = ar[i];
+		int j = i;
+		while (j > left && tmp < ar[j - gap])
+		{
+			ar[j] = ar[j - gap];
+			j -= gap;
+		}
+		ar[j] = tmp;
+	}
+}
+
+int dlta[] = { 5, 3, 2, 1 };
+void ShellSort(int* ar, int left, int right)
+{
+	int n = sizeof(dlta) / sizeof(dlta[0]); //增量的个数
+	for (int i = 0; i < n; ++i)
+	{
+		_ShellInsert(ar, left, right, dlta[i]);
+	}
+}
+
 void TestSortEfficient()
 {
-	int n = 10000;
+	int n = 20000;
 	int* a = (int*)malloc(sizeof(int) * n);
 	int* a1 = (int*)malloc(sizeof(int) * n);
 	int* a2 = (int*)malloc(sizeof(int) * n);
+	int* a3 = (int*)malloc(sizeof(int) * n);
+	int* a4 = (int*)malloc(sizeof(int) * n);
+	int* a5 = (int*)malloc(sizeof(int) * n);
+	int* a6 = (int*)malloc(sizeof(int) * n);
 	srand(time(0));
 	for (int i = 0; i < n; ++i)
 	{
 		a[i] = rand();
 		a1[i] = a[i];
 		a2[i] = a[i];
+		a3[i] = a[i];
+		a4[i] = a[i];
+		a5[i] = a[i];
+		a6[i] = a[i];
 	}
 
 	time_t start = clock();
@@ -83,6 +197,26 @@ void TestSortEfficient()
 	InsertSort_3(a2, 0, n);
 	end = clock();
 	printf("InsertSort_3: %u\n", (unsigned int)(end - start));
+
+	start = clock();
+	InsertSort_4(a3, 0, n);
+	end = clock();
+	printf("InsertSort_4: %u\n", (unsigned int)(end - start));
+
+	start = clock();
+	BinInsertSort(a4, 0, n);
+	end = clock();
+	printf("BinInsertSort: %u\n", (unsigned int)(end - start));
+
+	start = clock();
+	TwoWayInsertSort(a5, 0, n);
+	end = clock();
+	printf("TowWayInsertSort: %u\n", (unsigned int)(end - start));
+
+	start = clock();
+	ShellSort(a6, 0, n);
+	end = clock();
+	printf("ShellSort: %u\n", (unsigned int)(end - start));
 }
 
 
