7d6cddc2927454977a24082320504444f2a815fe
lihan0621 <73743214+lihan0621@users.noreply.github.com>
2021-12-11 17:15:57 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,32 +1,27 @@
-#include<stdio.h>
-#include<assert.h>
-#include<ctype.h>
-void PasswordTranslation(char* p, char* q)
+#include <stdio.h>
+
+int main1()
 {
-	while (*p != '\0')
+	int i, j, k;
+	int count=0;
+	for(i=1; i<=9; i++) 
 	{
-		if (islower(*p))
+		for(j=0; j<=9; j++) 
 		{
-			*q = *p - 32;
+			if(i==j) 
+			{
+				continue;
+			}
+			for(k=0; k<=9; k++) 
+			{
+				if(k!=i && k!=j) 
+				{
+					count++;
+				}
+			}
 		}
-		else if (isupper(*p))
-		{
-			*q = *p + 32;
-		}
-		else
-		{
-			*q = *p;
-		}
-		p++;
-		q++;
 	}
-	*q = '\0';
-}
-int main()
-{
-	char ar[] = "yahpingLIJIANLI";
-	char br[100]="";
-	PasswordTranslation(ar, br);
-	printf("%s\n", br);
+	printf("%d\n", count);
+	system("pause");
 	return 0;
 }
